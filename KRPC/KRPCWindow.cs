f679fa0234548a3fd9f44220c9d3ba76ed8ce4fd
djungelorm <djungelorm@users.noreply.github.com>
2014-02-12 21:08:46 +0000

    Add GUI to start/stop the server

---
+++ KRPC/KRPCWindow.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using UnityEngine;
+using KSP;
+using KRPC.Server;
+
+namespace KRPC
+{
+	[KSPAddon(KSPAddon.Startup.Flight, false)]
+	public class KRPCWindow : MonoBehaviour
+	{
+		private int windowId = UnityEngine.Random.Range(1000, 2000000);
+		private Rect windowPosition = new Rect();
+		private GUIStyle windowStyle, labelStyle;
+
+		public void Awake() {
+			windowStyle = new GUIStyle(HighLogic.Skin.window);
+			windowStyle.fixedWidth = 250f;
+			labelStyle = new GUIStyle(HighLogic.Skin.label);
+			labelStyle.stretchWidth = true;
+			RenderingManager.AddToPostDrawQueue(5, DrawGUI);
+		}
+
+		private void DrawGUI() {
+			windowPosition = GUILayout.Window (windowId, windowPosition, DrawWindow, "kRPC Server", windowStyle);
+		}
+
+		private void DrawWindow(int windowID) {
+			GUILayout.BeginVertical();
+			GUILayout.Label ("Server status: " + (KRPCAddon.Server.Running ? "Online" : "Offline"), labelStyle);
+			if (KRPCAddon.Server.Running) {
+				if (GUILayout.Button ("Stop server"))
+					KRPCAddon.Server.Stop ();
+				TCPServer tcpServer = (TCPServer)KRPCAddon.Server.Server;
+				GUILayout.Label ("Port: " + tcpServer.Port, labelStyle);
+				GUILayout.Label ("Allowed client(s): " + EndPointToString(tcpServer.EndPoint), labelStyle);
+				GUILayout.Label (tcpServer.GetConnectedClientIds().Count + " clients connected", labelStyle);
+			} else {
+				if (GUILayout.Button ("Start server"))
+					KRPCAddon.Server.Start ();
+			}
+			GUILayout.EndVertical ();
+			GUI.DragWindow ();
+		}
+
+		private string EndPointToString(IPAddress endPoint) {
+			if (endPoint.ToString() == "0.0.0.0")
+				return "any";
+			else if (endPoint.ToString() == "127.0.0.1")
+				return "local";
+			return endPoint.ToString ();
+		}
+	}
+}
+
