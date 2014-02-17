568428259e748be88878f309151f4357f8d61868
djungelorm <djungelorm@users.noreply.github.com>
2014-02-17 20:41:02 +0000

    Add unit test for default config values

---
+++ src/kRPCTest/KRPCConfigurationTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Net;
+using KRPC;
+
+namespace KRPCTest
+{
+    [TestFixture]
+    public class KRPCConfigurationTest
+    {
+        [Test]
+        public void DefaultConfig ()
+        {
+            var config = new KRPCConfiguration ("settings.cfg");
+            Assert.AreEqual(IPAddress.Loopback, config.Address);
+            Assert.AreEqual(50000, config.Port);
+        }
+    }
+}
+
