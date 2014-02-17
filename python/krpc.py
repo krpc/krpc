88f5de131e4ece109f165a288339f8373652e7f3
djungelorm <djungelorm@users.noreply.github.com>
2014-02-17 02:46:17 +0000

    Rewrite python client to make use of Services API

--- python/krpc.py
+++ python/krpc.py
@@ -1,90 +1,120 @@
-import proto.RPC_pb2 as rpc
-import proto.Control_pb2 as schema_control
-import proto.Orbit_pb2 as schema_orbit
+import proto.KRPC
 import socket
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 50000
+DEFAULT_ADDRESS = '127.0.0.1'
+DEFAULT_PORT = 50000
 BUFFER_SIZE = 4096
 
-class Client(object):
-    def __init__(self):
-        self._conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._conn.connect((TCP_IP, TCP_PORT))
-        self._conn.send(bytearray([0x48,0x45,0x4C,0x4C,0x4F,0xBA,0xDA,0x55]))
-        self._conn.send(bytearray([0x00]*32))
+class Logger(object):
+    @classmethod
+    def write(self, *args):
+        print ' '.join(str(x) for x in args)
 
-    def sendRequest(self, request):
-        """ Send an rpc.Request """
+class BaseService(object):
+    """ Abstract base class for all services. """
+    def __init__(self, client, name):
+        self._client = client
+        self._name = name
+
+    def _invoke(self, method, parameter = None, return_type = None, **kwargs):
+        return self._client._invoke(self._name, method, parameter, return_type, **kwargs)
+
+class KRPCService(BaseService):
+    """ Service for server related calls. For example, querying for the available services. """
+    def __init__(self, client):
+        super(KRPCService, self).__init__(client, 'KRPC')
+
+    def GetServices(self):
+        """ Get available services and methods """
+        return self._invoke('GetServices', return_type='KRPC.Services')
+
+class Service(BaseService):
+    """ A service, created from information received by querying the server """
+    def __init__(self, client, service):
+        """ Create a service from a KRPC.Service object """
+        Logger.write('Registering service', service.name)
+        super(Service, self).__init__(client, service.name)
+        self._name = service.name
+        for method in service.methods:
+            self._add_method(method)
+
+    def _add_method(self, method):
+        """ Adds a method to this service, from a KRPC.Method object """
+        # TODO: make the callback validate the parameter type
+        has_parameter_type = method.HasField('parameter_type')
+        has_return_type = method.HasField('return_type')
+        Logger.write('Registering method', method.name, '(', method.parameter_type, '->', method.return_type, ')')
+        if (not has_parameter_type) and (not has_return_type):
+            fn = lambda: self._invoke(method.name)
+        elif has_parameter_type and (not has_return_type):
+            fn = lambda parameter: self._invoke(method.name, parameter=parameter)
+        elif (not has_parameter_type) and has_return_type:
+            fn = lambda: self._invoke(method.name, return_type=method.return_type)
+        else: # method.has_parameter_type and method.has_return_type:
+            fn = lambda parameter: self._invoke(method.name, parameter=parameter, return_type=method.return_type)
+        self.__dict__[method.name] = fn
+
+class Client(object):
+    def __init__(self, connection):
+        self._connection = connection
+        self.KRPC = KRPCService(self)
+        for service in self.KRPC.GetServices().services:
+            if service.name != 'KRPC':
+                self.__dict__[service.name] = Service(self, service)
+
+    def _invoke(self, service, method, parameter=None, return_type=None, **kwargs):
+        Logger.write('Invoke', service+'.'+method, '(', type(parameter), '->', return_type, ')')
+
+        request = proto.KRPC.Request()
+        request.service = service
+        request.method = method
+        if parameter != None:
+            request.request = parameter.SerializeToString()
+
+        self._send_request(request)
+        response = self._receive_response()
+
+        if response.HasField('error'):
+            Logger.write('Error returned')
+            raise RuntimeError(response.error)
+
+        result = None
+
+        if return_type != None:
+            Logger.write('Got return value')
+            package,message_type = return_type.split('.')
+            result = proto.__dict__[package].__dict__[message_type]()
+            result.ParseFromString(response.response)
+        else:
+            Logger.write('No return value')
+
+        return result
+
+    def _send_request(self, request):
+        """ Send a KRPC.Request object to the server """
         from google.protobuf.internal import encoder
         data = request.SerializeToString()
         delimiter = encoder._VarintBytes(len(data))
-        self._conn.send(delimiter)
-        self._conn.send(data)
+        self._connection.send(delimiter)
+        self._connection.send(data)
 
-    def getResponse(self):
-        """ Receive and return an rpc.Response """
+    def _receive_response(self):
         import google.protobuf.internal.decoder as decoder
-        data = self._conn.recv(BUFFER_SIZE)
+        data = self._connection.recv(BUFFER_SIZE)
+        # FIXME: we might not receive all of the data in one go
         (size, position) = decoder._DecodeVarint(data, 0)
-        response = rpc.Response()
+        response = proto.KRPC.Response()
         response.ParseFromString(data[position:position+size])
         return response
 
-class Service(object):
-    def __init__(self, service, client):
-        self._service = service
-        self._client = client
-
-    def execute(self, method, data = None):
-        request = rpc.Request()
-        request.service = self._service
-        request.method = method
-        if data != None:
-            request.request = data.SerializeToString()
-        self._client.sendRequest(request)
-        response = self._client.getResponse()
-        if response.error:
-            raise RuntimeError(response.message.replace('\r','\n'))
-        return response.response
-
-class Control(Service):
-    def __init__(self, client):
-        super(Control, self).__init__("Control", client)
-
-    def Set(self,
-            throttle=None,
-            pitch=None,
-            yaw=None,
-            roll=None,
-            x=None,
-            y=None,
-            z=None,
-            sas=None,
-            rcs=None):
-        controls = schema_control.Controls()
-        if throttle != None:
-            controls.throttle = throttle
-        if sas != None:
-            controls.sas = sas
-        #TODO: implement fully
-        self.execute("Set", controls)
-
-    def Get(self):
-        response = self.execute("Get")
-        result = schema_control.Controls()
-        result.ParseFromString(response)
-        return result
-
-    def ActivateNextStage(self):
-        self.execute("ActivateNextStage")
-
-class Orbit(Service):
-    def __init__(self, client):
-        super(Orbit, self).__init__("Orbit", client)
-
-    def Get(self):
-        response = self.execute("Get");
-        result = schema_orbit.OrbitData()
-        result.ParseFromString(response)
-        return result
+def connect(address=DEFAULT_ADDRESS, port=DEFAULT_PORT, name=None):
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connection.connect((address, port))
+    connection.send(bytearray([0x48,0x45,0x4C,0x4C,0x4F,0xBA,0xDA,0x55]))
+    name_bytes = bytearray(name, 'utf_8')
+    identifier = bytearray(32)
+    for i,x in enumerate(name_bytes):
+        identifier[i] = x
+    # TODO: handle identifiers that are too long
+    connection.send(identifier)
+    return Client(connection)
