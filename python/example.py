3c4e445a5c74ea4f21905ec7753eed1efd3045f3
djungelorm <djungelorm@users.noreply.github.com>
2014-02-17 02:52:05 +0000

    Update

--- python/example.py
+++ python/example.py
@@ -3,20 +3,21 @@
 import krpc
 import time
 
-client = krpc.Client()
+client = krpc.connect(name='Example script');
+
 control = krpc.Control(client)
 orbit = krpc.Orbit(client)
 
-control.Set(throttle=1.0, sas=True)
+client.Control.Set(throttle=1.0, sas=True)
 time.sleep(1.0)
-control.ActivateNextStage()
+client.Control.ActivateNextStage()
 time.sleep(30)
-control.ActivateNextStage()
+client.Control.ActivateNextStage()
 
 time.sleep(180-30)
-control.ActivateNextStage()
+client.Control.ActivateNextStage()
 
 while True:
-    orbitData = orbit.Get()
+    orbitData = client.Orbit.Get()
     print '%.0f x %.0f' % (orbitData.apoapsis, orbitData.periapsis)
     time.sleep(0.5)
