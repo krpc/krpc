11f6fc10d7372d5c50595092025573669b662a2d
djungelorm <djungelorm@users.noreply.github.com>
2014-02-19 00:55:21 +0000

    Expand the example script

--- python/example.py
+++ python/example.py
@@ -3,22 +3,78 @@
 import krpc
 import time
 
-client = krpc.connect(name='Example script');
-print 'Connected to server, version', client.KRPC.GetStatus().version
+def main():
+    # Connect to the server with the default settings
+    # (IP address 127.0.0.1 and port 50000)
+    ksp = krpc.connect(name='Example script')
+    print 'Connected to server, version', ksp.KRPC.GetStatus().version
 
-control = krpc.Control(client)
-orbit = krpc.Orbit(client)
+    # Set the throttle to 100% and enable SAS
+    controls = ksp.Control.GetControlInputs()
+    controls.throttle = 1
+    controls.sas = True
+    ksp.Control.SetControlInputs(controls)
 
-client.Control.Set(throttle=1.0, sas=True)
-time.sleep(1.0)
-client.Control.ActivateNextStage()
-time.sleep(30)
-client.Control.ActivateNextStage()
+    # Countdown...
+    print '3'; time.sleep(1)
+    print '2'; time.sleep(1)
+    print '1'; time.sleep(1)
 
-time.sleep(180-30)
-client.Control.ActivateNextStage()
+    # Activate the first stage
+    print 'Launch!'
+    ksp.Control.ActivateNextStage()
 
-while True:
-    orbitData = client.Orbit.Get()
-    print '%.0f x %.0f' % (orbitData.apoapsis, orbitData.periapsis)
-    time.sleep(0.5)
+    # Ascend to 10km
+    print 'Vertical Ascent...'
+    srbs_separated = False
+    while True:
+
+        # Check altitude
+        altitude = ksp.Flight.GetFlightData().altitude
+        print '  Altitude = %.1f km' % (altitude/1000)
+        if altitude > 10000:
+            break
+
+        # Check if the solid boosters need to be ditched
+        # We assume this will happen before we reach 10km
+        if not srbs_separated:
+            resources = ksp.Vessel.GetResources()
+            print '  Solid fuel = %.1f T' % resources.solidFuel
+            if resources.solidFuel < 0.1:
+                print '  SRB separation!'
+                ksp.Control.ActivateNextStage()
+                srbs_separated = True
+
+        time.sleep(1)
+
+    print 'Gravity turn...'
+    # Pitch the vessel west for 4 seconds, then hold position using SAS
+    # TODO: this doesn't work
+    controls.sas = False
+    controls.pitch = 1
+    ksp.Control.SetControlInputs(controls)
+    time.sleep(3)
+    controls.sas = True
+    controls.pitch = 0
+    ksp.Control.SetControlInputs(controls)
+
+    # Raise apoapsis to above 80km
+    while True:
+
+        # Apoapsis is relative to the center of Kerbin, so subtract 600km
+        apoapsis = ksp.Orbit.GetOrbitData().apoapsis - 600000
+        print '  Apoapsis = %.1f km' % (apoapsis/1000)
+        if apoapsis > 80000:
+            break
+
+        time.sleep(1)
+
+    # Disable the control inputs
+    controls.sas = False
+    controls.throttle = 0
+    ksp.Control.SetControlInputs(controls)
+
+    print 'Program complete'
+
+if __name__ == "__main__":
+    main()
