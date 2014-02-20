19e799c97829504e50c17e7576089c248f3f5d6a
djungelorm <djungelorm@users.noreply.github.com>
2014-02-20 22:05:10 +0000

    Add ProtocolBuffers.IsAValueType and .GetValueTypeName

--- src/kRPC/Utils/ProtocolBuffers.cs
+++ src/kRPC/Utils/ProtocolBuffers.cs
@@ -7,7 +7,8 @@ namespace KRPC.Utils
     class ProtocolBuffers
     {
         /// <summary>
-        /// Return the string name of the C# type for the Protocol Buffer message type
+        /// Return the string name of the Protocol Buffer message type (with the package name prefixing it).
+        /// E.g. "KRPC.Request"
         /// </summary>
         public static string GetMessageTypeName (Type type) {
             if (type == null)
@@ -15,6 +16,37 @@ namespace KRPC.Utils
             if (!IsAMessageType (type))
                 throw new ArgumentException (type.ToString () + " is not a Protocol Buffer message type");
             return type.FullName.Replace("KRPC.Schema.", "");
+        }
+
+        /// <summary>
+        /// Return the string name of the Protocol Buffer message type corresponding to the given type.
+        /// E.g. "uint32" for uint
+        /// </summary>
+        public static string GetValueTypeName (Type type)
+        {
+            // Note: C# has no equivalent types for sint32, sint64, fixed32, fixed64, sfixed32 or sfixed64
+            if (type == null)
+                throw new ArgumentException ("null is not a Protocol Buffer value type");
+            else if (type == typeof(double))
+                return "double";
+            else if (type == typeof(float))
+                return "float";
+            else if (type == typeof(int))
+                return "int32";
+            else if (type == typeof(long))
+                return "int64";
+            else if (type == typeof(uint))
+                return "uint32";
+            else if (type == typeof(ulong))
+                return "uint64";
+            else if (type == typeof(bool))
+                return "bool";
+            else if (type == typeof(string))
+                return "string";
+            else if (type == typeof(byte[]))
+                return "bytes";
+            else
+                throw new ArgumentException (type.ToString() + " is not a Protocol Buffer value type");
         }
 
         /// <summary>
@@ -37,6 +69,20 @@ namespace KRPC.Utils
         public static bool IsAMessageType (Type type)
         {
             return typeof(IMessage).IsAssignableFrom (type);
+        }
+
+        public static bool IsAValueType (Type type)
+        {
+            return
+                type == typeof(double) ||
+                type == typeof(float) ||
+                type == typeof(int) ||
+                type == typeof(long) ||
+                type == typeof(uint) ||
+                type == typeof(ulong) ||
+                type == typeof(bool) ||
+                type == typeof(string) ||
+                type == typeof(byte[]);
         }
     }
 }
--- src/kRPCTest/Utils/ProtocolBuffersTest.cs
+++ src/kRPCTest/Utils/ProtocolBuffersTest.cs
@@ -18,6 +18,30 @@ namespace KRPCTest.Utils
         }
 
         [Test]
+        public void GetValueTypeName ()
+        {
+            Assert.AreEqual ("double", ProtocolBuffers.GetValueTypeName (typeof (double)));
+            Assert.AreEqual ("float", ProtocolBuffers.GetValueTypeName (typeof (float)));
+            Assert.AreEqual ("int32", ProtocolBuffers.GetValueTypeName (typeof (int)));
+            Assert.AreEqual ("int64", ProtocolBuffers.GetValueTypeName (typeof (long)));
+            Assert.AreEqual ("uint32", ProtocolBuffers.GetValueTypeName (typeof (uint)));
+            Assert.AreEqual ("uint64", ProtocolBuffers.GetValueTypeName (typeof (ulong)));
+            //Assert.AreEqual ("sint32", ProtocolBuffers.GetValueTypeName (typeof()));
+            //Assert.AreEqual ("sint64", ProtocolBuffers.GetValueTypeName (typeof()));
+            //Assert.AreEqual ("fixed32", ProtocolBuffers.GetValueTypeName (typeof()));
+            //Assert.AreEqual ("fixed64", ProtocolBuffers.GetValueTypeName (typeof()));
+            //Assert.AreEqual ("sfixed32", ProtocolBuffers.GetValueTypeName (typeof()));
+            //Assert.AreEqual ("sfixed64", ProtocolBuffers.GetValueTypeName (typeof()));
+            Assert.AreEqual ("bool", ProtocolBuffers.GetValueTypeName (typeof (bool)));
+            Assert.AreEqual ("string", ProtocolBuffers.GetValueTypeName (typeof (string)));
+            Assert.AreEqual ("bytes", ProtocolBuffers.GetValueTypeName (typeof (byte[])));
+            Assert.Throws<ArgumentException> (() => ProtocolBuffers.GetValueTypeName (typeof(KRPC.Schema.KRPC.Request)));
+            Assert.Throws<ArgumentException> (() => ProtocolBuffers.GetValueTypeName (typeof (KRPC.Schema.KRPC.Response)));
+            Assert.Throws<ArgumentException> (() => ProtocolBuffers.GetValueTypeName (null));
+            Assert.Throws<ArgumentException> (() => ProtocolBuffers.GetValueTypeName (typeof (ProtocolBuffersTest)));
+        }
+
+        [Test]
         public void BuilderForMessageType ()
         {
             Assert.AreEqual (typeof(KRPC.Schema.KRPC.Request.Builder),
@@ -37,6 +61,24 @@ namespace KRPCTest.Utils
             Assert.IsFalse (ProtocolBuffers.IsAMessageType (null));
             Assert.IsFalse (ProtocolBuffers.IsAMessageType (typeof (string)));
             Assert.IsFalse (ProtocolBuffers.IsAMessageType (typeof (ProtocolBuffersTest)));
+        }
+
+        [Test]
+        public void IsAValueType ()
+        {
+            Assert.IsTrue (ProtocolBuffers.IsAValueType (typeof (double)));
+            Assert.IsTrue (ProtocolBuffers.IsAValueType (typeof (float)));
+            Assert.IsTrue (ProtocolBuffers.IsAValueType (typeof (int)));
+            Assert.IsTrue (ProtocolBuffers.IsAValueType (typeof (long)));
+            Assert.IsTrue (ProtocolBuffers.IsAValueType (typeof (uint)));
+            Assert.IsTrue (ProtocolBuffers.IsAValueType (typeof (ulong)));
+            Assert.IsTrue (ProtocolBuffers.IsAValueType (typeof (bool)));
+            Assert.IsTrue (ProtocolBuffers.IsAValueType (typeof (string)));
+            Assert.IsTrue (ProtocolBuffers.IsAValueType (typeof (byte[])));
+            Assert.IsFalse (ProtocolBuffers.IsAValueType (typeof (KRPC.Schema.KRPC.Request)));
+            Assert.IsFalse (ProtocolBuffers.IsAValueType (typeof (KRPC.Schema.KRPC.Response)));
+            Assert.IsFalse (ProtocolBuffers.IsAValueType (null));
+            Assert.IsFalse (ProtocolBuffers.IsAValueType (typeof (ProtocolBuffersTest)));
         }
     }
 }
