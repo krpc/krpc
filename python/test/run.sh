f18b97f36963c66526f6e4d62561601ec1d2d90a
djungelorm <djungelorm@users.noreply.github.com>
2014-02-23 21:33:38 +0000

    Add files to clean target, rename logging file

--- python/test/run.sh
+++ python/test/run.sh
@@ -3,7 +3,7 @@
 TEST_SERVER=../../src/TestServer/bin/Debug/TestServer.exe
 
 test -f $TEST_SERVER || make -C ../.. CSHARP_CONFIG=Debug TestServer
-$TEST_SERVER 1>test-server.log &
+$TEST_SERVER 1>server.log &
 PID=$!
 echo "Test server running as process $PID"
 # Wait for server to start properly
