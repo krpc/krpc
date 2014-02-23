6a2362063a8e6f74847c30f60e90e1e2dc2e0bc2
djungelorm <djungelorm@users.noreply.github.com>
2014-02-23 21:34:26 +0000

    Add server tests

--- python/test/test_server.py
+++ python/test/test_server.py
@@ -6,9 +6,22 @@ import krpc
 
 class TestServer(unittest.TestCase):
 
-    def test_connect(self):
-        ksp = krpc.connect(name='TestServer')
-        self.assertEqual('42', ksp.TestService.Int32ToString(42))
+    def setUp(self):
+        self.ksp = krpc.connect(name='TestServer')
+
+    def test_value_parameters(self):
+        self.assertEqual('3.14159', self.ksp.TestService.FloatToString(float(3.14159)))
+        self.assertEqual('42', self.ksp.TestService.Int32ToString(42))
+        self.assertEqual('123456789000', self.ksp.TestService.Int64ToString(123456789000L))
+        self.assertEqual('True', self.ksp.TestService.BoolToString(True))
+        self.assertEqual('False', self.ksp.TestService.BoolToString(False))
+        self.assertEqual(12345, self.ksp.TestService.StringToInt32('12345'))
+
+    def test_multiple_value_parameters(self):
+        self.assertEqual('3.14159', self.ksp.TestService.AddMultipleValues(0.14159, 1, 2))
+
+    def test_incorrect_parameter_type(self):
+        self.ksp.TestService.FloatToString(42)
 
 if __name__ == '__main__':
     unittest.main()
