cb78d0d08136d746208a84ce49adb263cb05d079
djungelorm <djungelorm@users.noreply.github.com>
2014-03-09 19:47:51 +0000

    Add more orbit test cases

--- test/test_orbit.py
+++ test/test_orbit.py
@@ -35,5 +35,33 @@ class TestOrbit(testingtools.TestCase):
         self.assertClose(241.186299925129, self.ksp.Orbit.ArgumentOfPeriapsis)
         self.assertClose(2.30395571797464, self.ksp.Orbit.MeanAnomalyAtEpoch)
 
+    def test_orbit_mun_escape_soi(self):
+        load_save('orbit-mun-escape-soi')
+        self.ksp = krpc.connect()
+        self.assertEqual("Mun", self.ksp.Orbit.Body)
+        self.assertClose(828815.0308573832, self.ksp.Orbit.Periapsis)
+        self.assertClose(2659937.0232935967, self.ksp.Orbit.Apoapsis)
+        self.assertClose(628815.0308573832, self.ksp.Orbit.PeriapsisAltitude)
+        self.assertClose(2459937.0232935967, self.ksp.Orbit.ApoapsisAltitude)
+        self.assertClose(0.524864468444386, self.ksp.Orbit.Eccentricity)
+        self.assertClose(0, self.ksp.Orbit.Inclination)
+        self.assertClose(12.895673289558, self.ksp.Orbit.LongitudeOfAscendingNode)
+        self.assertClose(67.3969257458513, self.ksp.Orbit.ArgumentOfPeriapsis)
+        self.assertClose(6.21020183993513, self.ksp.Orbit.MeanAnomalyAtEpoch)
+
+    def test_orbit_minmus_parabolic(self):
+        load_save('orbit-minmus-parabolic')
+        self.ksp = krpc.connect()
+        self.assertEqual("Minmus", self.ksp.Orbit.Body)
+        self.assertClose(87187.64537168786, self.ksp.Orbit.Periapsis)
+        self.assertClose(-175327.32795440647, self.ksp.Orbit.Apoapsis)
+        self.assertClose(27187.64537168786, self.ksp.Orbit.PeriapsisAltitude)
+        self.assertClose(-235327.32795440647, self.ksp.Orbit.ApoapsisAltitude)
+        self.assertClose(2.97839708101655, self.ksp.Orbit.Eccentricity)
+        self.assertClose(168.280967855609, self.ksp.Orbit.Inclination)
+        self.assertClose(181.171756205933, self.ksp.Orbit.LongitudeOfAscendingNode)
+        self.assertClose(165.50774557981, self.ksp.Orbit.ArgumentOfPeriapsis)
+        self.assertClose(-4.65482114687744, self.ksp.Orbit.MeanAnomalyAtEpoch)
+
 if __name__ == "__main__":
     unittest.main()
