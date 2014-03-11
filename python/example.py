7b10896b22a0e095aca76ff20eebe440791ce8da
djungelorm <djungelorm@users.noreply.github.com>
2014-03-11 01:17:35 +0000

    Update example script

--- python/example.py
+++ python/example.py
@@ -16,9 +16,13 @@ def main():
     ksp = krpc.connect(name='Example script')
     print 'Connected to server, version', ksp.KRPC.GetStatus().version
 
+    vessel = ksp.Flight.ActiveVessel
+    orbit = vessel.Orbit
+    control = ksp.Control
+
     # Set the throttle to 100% and enable SAS
-    ksp.Control.Throttle = 1
-    ksp.Control.SAS = True
+    control.Throttle = 1
+    control.SAS = True
 
     # Countdown...
     print '3'; time.sleep(1)
@@ -27,7 +31,7 @@ def main():
 
     # Activate the first stage
     print 'Launch!'
-    ksp.Control.ActivateNextStage()
+    control.ActivateNextStage()
 
     # Ascend to 10km and ditch SRBs when they're empty
     print 'Vertical ascent...'
@@ -35,7 +39,7 @@ def main():
     while True:
 
         # Check altitude, exit loop if higher than 10km
-        altitude = ksp.Flight.Altitude
+        altitude = vessel.Altitude
         print '  Altitude = %.1f km' % (altitude/1000)
         if altitude > 10000:
             break
@@ -43,29 +47,29 @@ def main():
         # Check if the solid boosters need to be ditched
         # (We assume this will happen before we reach 10km)
         if not srbs_separated:
-            resources = ksp.Vessel.GetResources()
-            print '  Solid fuel = %.1f T' % resources.solidFuel
-            if resources.solidFuel < 0.1:
+            solidFuel = vessel.GetResource('SolidFuel')
+            print '  Solid fuel = %.1f T' % solidFuel
+            if solidFuel < 0.1:
                 print '  SRB separation!'
-                ksp.Control.ActivateNextStage()
+                control.ActivateNextStage()
                 srbs_separated = True
 
         time.sleep(1)
 
     # Disable SAS, pitch the vessel to 50 degrees to the west, then hold position using SAS
     print 'Gravity turn...'
-    ksp.Control.SAS = False
-    ksp.Control.Yaw = 0.1
-    while ksp.Flight.Pitch > 50:
+    control.SAS = False
+    control.Yaw = 0.1
+    while vessel.Pitch > 50:
         time.sleep(0.25)
-    ksp.Control.Yaw = 0
-    ksp.Control.SAS = True
+    control.Yaw = 0
+    control.SAS = True
 
     # Raise apoapsis to above 80km
     while True:
 
         # Apoapsis is relative to the center of Kerbin, so subtract 600km
-        apoapsis = ksp.Orbit.Apoapsis - 600000
+        apoapsis = orbit.Apoapsis - 600000
         print '  Apoapsis = %.1f km' % (apoapsis/1000)
         if apoapsis > 80000:
             break
@@ -74,12 +78,12 @@ def main():
 
     # Disable the control inputs and coast to apoapsis
     print 'Coasting to apoapsis...'
-    ksp.Control.SAS = False
-    ksp.Control.Throttle = 0
+    control.SAS = False
+    control.Throttle = 0
     while True:
 
         # Check altitude, exit loop if higher than 79km
-        altitude = ksp.Flight.Altitude
+        altitude = vessel.Altitude
         print '  Altitude = %.1f km' % (altitude/1000)
         if altitude > 79000:
             break
@@ -87,26 +91,26 @@ def main():
 
     # Circularize the orbit
     print 'Circularizing...'
-    ksp.Control.SAS = False
-    ksp.Control.Yaw = 0.1
-    while ksp.Flight.Pitch > 0:
+    control.SAS = False
+    control.Yaw = 0.1
+    while vessel.Pitch > 0:
         time.sleep(0.25)
-    ksp.Control.Yaw = 0
-    ksp.Control.SAS = True
+    control.Yaw = 0
+    control.SAS = True
     # Raise periapsis until eccentricity starts the increase (happens just after reaching 0)
-    ksp.Control.Throttle = 1
+    control.Throttle = 1
 
-    eccentricity = ksp.Orbit.Eccentricity
+    eccentricity = orbit.Eccentricity
     time.sleep(1)
 
     while True:
 
-        apoapsis = ksp.Orbit.Apoapsis - 600000
-        periapsis = ksp.Orbit.Periapsis - 600000
+        apoapsis = orbit.Apoapsis - 600000
+        periapsis = orbit.Periapsis - 600000
         prevEccentricity = eccentricity;
-        eccentricity = ksp.Orbit.Eccentricity
+        eccentricity = orbit.Eccentricity
 
-        print '  Orbit = %.1f km x %.1f km (e = %.3f)' % ((apoapsis/1000), (periapsis/1000), ksp.Orbit.Eccentricity)
+        print '  Orbit = %.1f km x %.1f km (e = %.3f)' % ((apoapsis/1000), (periapsis/1000), orbit.Eccentricity)
 
         print (prevEccentricity - eccentricity)
         if prevEccentricity - eccentricity < 0:
@@ -116,11 +120,11 @@ def main():
             time.sleep(1)
         else:
             # We are close, so reduce throttle and check eccentricity more frequently
-            ksp.Control.Throttle = 0.1
+            control.Throttle = 0.1
             time.sleep(0.2)
 
-    ksp.Control.Throttle = 0
-    ksp.Control.SAS = False
+    control.Throttle = 0
+    control.SAS = False
 
     print 'Program complete'
 
