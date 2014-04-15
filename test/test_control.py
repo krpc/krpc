145ce9413a05c1c1ad87850174fbbb7ce38c0a10
djungelorm <djungelorm@users.noreply.github.com>
2014-04-15 18:26:32 +0100

    Update control test

--- test/test_control.py
+++ test/test_control.py
@@ -11,50 +11,48 @@ class TestControl(testingtools.TestCase):
     def setUp(self):
         load_save('flight')
         self.ksp = krpc.connect()
-        self.vessel = self.ksp.SpaceCenter.ActiveVessel
-        self.control = self.vessel.Control
-        self.flight = self.vessel.Flight
+        self.vessel = self.ksp.space_center.active_vessel
+        self.control = self.vessel.control
+        self.flight = self.vessel.flight
 
     def test_pitch_control(self):
-        self.control.SAS = False
-        self.control.Pitch = 1
+        self.control.sas = False
+        self.control.pitch = 1
         time.sleep(3)
-        self.control.Pitch = 0
+        self.control.pitch = 0
 
         # Check flight is pitching in correct direction
-        pitch = self.flight.Pitch
+        pitch = self.flight.pitch
         time.sleep(0.1)
-        diff = pitch - self.flight.Pitch
+        diff = pitch - self.flight.pitch
         self.assertGreater(diff, 0)
 
     def test_yaw_control(self):
-        self.control.SAS = False
-        self.control.Yaw = 1
+        self.control.sas = False
+        self.control.yaw = 1
         time.sleep(3)
-        self.control.Yaw = 0
+        self.control.yaw = 0
 
         # Check flight is yawing in correct direction
-        heading = self.flight.Heading
+        heading = self.flight.heading
         time.sleep(0.1)
-        diff = heading - self.flight.Heading
+        diff = self.flight.heading - heading
         self.assertGreater(diff, 0)
 
     def test_roll_control(self):
-        self.control.SAS = False
-        self.control.Roll = 0.1
+        self.control.sas = False
+        self.control.roll = 0.1
         time.sleep(3)
-        self.control.Roll = 0
+        self.control.roll = 0
 
-        self.assertBetween(57, 58, self.flight.Pitch)
-        self.assertBetween(224, 227, self.flight.Heading)
-        self.assertBetween(80, 110, self.flight.Roll)
+        self.assertClose(23, self.flight.pitch, error=1)
+        self.assertClose(115, self.flight.heading, error=1)
 
         # Check flight is rolling in correct direction
-        roll = self.flight.Roll
+        roll = self.flight.roll
         time.sleep(0.1)
-        diff = roll - self.flight.Roll
+        diff = roll - self.flight.roll
         self.assertGreater(diff, 0)
-
 
 if __name__ == "__main__":
     unittest.main()
