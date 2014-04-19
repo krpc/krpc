c75bf45ee095ff7c560404a7a15d85a0ae2b3ab1
djungelorm <djungelorm@users.noreply.github.com>
2014-04-19 12:11:47 +0100

    Update resources api implementation

--- src/kRPCServices/Services/Resources.cs
+++ src/kRPCServices/Services/Resources.cs
@@ -10,32 +10,58 @@ namespace KRPCServices.Services
     [KRPCClass (Service = "SpaceCenter")]
     public class Resources
     {
-        //global::Vessel vessel;
+        global::Vessel vessel;
+
         internal Resources (global::Vessel vessel)
         {
-            //this.vessel = vessel;
+            this.vessel = vessel;
+        }
+
+        List<PartResource> GetResources (uint stage = 0)
+        {
+            var resources = new List<PartResource> ();
+            foreach (Part part in vessel.Parts) {
+                if (stage == 0 || part.inverseStage == stage) {
+                    foreach (PartResource resource in part.Resources)
+                        resources.Add (resource);
+                }
+            }
+            return resources;
         }
         //FIXME: what return type?
-        //        [KRPCProperty]
-        //        public string[] ResourceNames {
-        //            get { throw new NotImplementedException (); }
-        //        }
+        //[KRPCProperty]
+        //public string[] ResourceNames {
+        //    get { throw new NotImplementedException (); }
+        //}
         [KRPCMethod]
         public bool HasResource (string name)
         {
-            throw new NotImplementedException ();
+            foreach (var resource in GetResources())
+                if (resource.resourceName == name)
+                    return true;
+            return false;
         }
 
         [KRPCMethod]
         public double Max (string name, uint stage = 0)
         {
-            throw new NotImplementedException ();
+            double amount = 0;
+            foreach (var resource in GetResources(stage)) {
+                if (resource.resourceName == name)
+                    amount += resource.maxAmount;
+            }
+            return amount;
         }
 
         [KRPCMethod]
         public double Amount (string name, uint stage = 0)
         {
-            throw new NotImplementedException ();
+            double amount = 0;
+            foreach (var resource in GetResources(stage)) {
+                if (resource.resourceName == name)
+                    amount += resource.amount;
+            }
+            return amount;
         }
 
         [KRPCMethod]
@@ -43,28 +69,5 @@ namespace KRPCServices.Services
         {
             throw new NotImplementedException ();
         }
-        //[KRPCMethod]
-        //public double GetResource (string name)
-        //{
-        //    // Get all resources
-        //    var resources = new List<PartResource> ();
-        //    foreach (Part part in vessel.Parts) {
-        //        foreach (PartResource resource in part.Resources) {
-        //            resources.Add (resource);
-        //        }
-        //    }
-        //    return GetResourceAmount (name, resources);
-        //}
-        //
-        //double GetResourceAmount (string name, ICollection<PartResource> resources)
-        //{
-        //    double amount = 0;
-        //    foreach (var resource in resources) {
-        //        if (resource.resourceName == name) {
-        //            amount += resource.amount;
-        //        }
-        //    }
-        //    return amount;
-        //}
     }
 }
