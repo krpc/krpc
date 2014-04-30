fcb6ace42487fa6e31e984124bf4fe14fd3c392a
djungelorm <djungelorm@users.noreply.github.com>
2014-04-30 09:55:39 +0100

    Implement ParameterizedContinuations for up to 10 arguments

--- src/kRPC/Continuations/ParameterizedContinuation.cs
+++ src/kRPC/Continuations/ParameterizedContinuation.cs
@@ -1,46 +1,165 @@
 namespace KRPC.Continuations
 {
-    /// <summary>
-    /// A continuation with a single parameter of type TIn and a return value of
-    /// type TOut
-    /// </summary>
-    public sealed class ParameterizedContinuation<TIn,TOut> : Continuation<TOut>
-    {
-        public delegate TOut Fn (TIn data);
-        Fn fn;
-        TIn data;
+/*[[[cog
+import cog
+for i in range(int(nargs)):
+    cog.outl("""
+    public sealed class ParameterizedContinuation<""" + ','.join(['TReturn'] + ['TArg%d' % i for i in range(i+1)]) + """> : Continuation<TReturn> {
+        public delegate TReturn Fn (""" + ', '.join(['TArg%d arg%d' % (i,i) for i in range(i+1)]) + """);
+        Fn fn; """ + ' '.join(['TArg%d arg%d;' % (i,i) for i in range(i+1)]) + """
+        public ParameterizedContinuation (Fn fn, """ + ', '.join(['TArg%d arg%d' % (i,i) for i in range(i+1)]) + ') { ' + \
+            'this.fn = fn; ' + ' '.join(['this.arg%d = arg%d;' % (i,i) for i in range(i+1)]) + """ }
+        public override TReturn Run () { return fn (""" + ', '.join(['arg%d' % i for i in range(i+1)]) + """); }
+    }""")
 
-        public ParameterizedContinuation (Fn fn, TIn data)
-        {
-            this.fn = fn;
-            this.data = data;
-        }
+    cog.outl("""
+    public sealed class ParameterizedContinuationVoid<""" + ','.join(['TArg%d' % i for i in range(i+1)]) + """> : Continuation {
+        public delegate void Fn (""" + ', '.join(['TArg%d arg%d' % (i,i) for i in range(i+1)]) + """);
+        Fn fn; """ + ' '.join(['TArg%d arg%d;' % (i,i) for i in range(i+1)]) + """
+        public ParameterizedContinuationVoid (Fn fn, """ + ', '.join(['TArg%d arg%d' % (i,i) for i in range(i+1)]) + ') { ' + \
+            'this.fn = fn; ' + ' '.join(['this.arg%d = arg%d;' % (i,i) for i in range(i+1)]) + """ }
+        public override void Run () { fn (""" + ', '.join(['arg%d' % i for i in range(i+1)]) + """); }
+    }""")
+]]]*/
 
-        public override TOut Run ()
-        {
-            return fn (data);
-        }
-    }
+public sealed class ParameterizedContinuation<TReturn,TArg0> : Continuation<TReturn> {
+    public delegate TReturn Fn (TArg0 arg0);
+    Fn fn; TArg0 arg0;
+    public ParameterizedContinuation (Fn fn, TArg0 arg0) { this.fn = fn; this.arg0 = arg0; }
+    public override TReturn Run () { return fn (arg0); }
+}
 
+public sealed class ParameterizedContinuationVoid<TArg0> : Continuation {
+    public delegate void Fn (TArg0 arg0);
+    Fn fn; TArg0 arg0;
+    public ParameterizedContinuationVoid (Fn fn, TArg0 arg0) { this.fn = fn; this.arg0 = arg0; }
+    public override void Run () { fn (arg0); }
+}
 
-    /// <summary>
-    /// A continuation with a single parameter of type TIn and no return value
-    /// </summary>
-    public sealed class ParameterizedContinuation<TIn> : Continuation
-    {
-        public delegate void Fn (TIn data);
-        Fn fn;
-        TIn data;
+public sealed class ParameterizedContinuation<TReturn,TArg0,TArg1> : Continuation<TReturn> {
+    public delegate TReturn Fn (TArg0 arg0, TArg1 arg1);
+    Fn fn; TArg0 arg0; TArg1 arg1;
+    public ParameterizedContinuation (Fn fn, TArg0 arg0, TArg1 arg1) { this.fn = fn; this.arg0 = arg0; this.arg1 = arg1; }
+    public override TReturn Run () { return fn (arg0, arg1); }
+}
 
-        public ParameterizedContinuation (Fn fn, TIn data)
-        {
-            this.fn = fn;
-            this.data = data;
-        }
+public sealed class ParameterizedContinuationVoid<TArg0,TArg1> : Continuation {
+    public delegate void Fn (TArg0 arg0, TArg1 arg1);
+    Fn fn; TArg0 arg0; TArg1 arg1;
+    public ParameterizedContinuationVoid (Fn fn, TArg0 arg0, TArg1 arg1) { this.fn = fn; this.arg0 = arg0; this.arg1 = arg1; }
+    public override void Run () { fn (arg0, arg1); }
+}
 
-        public override void Run ()
-        {
-            fn (data);
-        }
-    }
+public sealed class ParameterizedContinuation<TReturn,TArg0,TArg1,TArg2> : Continuation<TReturn> {
+    public delegate TReturn Fn (TArg0 arg0, TArg1 arg1, TArg2 arg2);
+    Fn fn; TArg0 arg0; TArg1 arg1; TArg2 arg2;
+    public ParameterizedContinuation (Fn fn, TArg0 arg0, TArg1 arg1, TArg2 arg2) { this.fn = fn; this.arg0 = arg0; this.arg1 = arg1; this.arg2 = arg2; }
+    public override TReturn Run () { return fn (arg0, arg1, arg2); }
+}
+
+public sealed class ParameterizedContinuationVoid<TArg0,TArg1,TArg2> : Continuation {
+    public delegate void Fn (TArg0 arg0, TArg1 arg1, TArg2 arg2);
+    Fn fn; TArg0 arg0; TArg1 arg1; TArg2 arg2;
+    public ParameterizedContinuationVoid (Fn fn, TArg0 arg0, TArg1 arg1, TArg2 arg2) { this.fn = fn; this.arg0 = arg0; this.arg1 = arg1; this.arg2 = arg2; }
+    public override void Run () { fn (arg0, arg1, arg2); }
+}
+
+public sealed class ParameterizedContinuation<TReturn,TArg0,TArg1,TArg2,TArg3> : Continuation<TReturn> {
+    public delegate TReturn Fn (TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3);
+    Fn fn; TArg0 arg0; TArg1 arg1; TArg2 arg2; TArg3 arg3;
+    public ParameterizedContinuation (Fn fn, TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3) { this.fn = fn; this.arg0 = arg0; this.arg1 = arg1; this.arg2 = arg2; this.arg3 = arg3; }
+    public override TReturn Run () { return fn (arg0, arg1, arg2, arg3); }
+}
+
+public sealed class ParameterizedContinuationVoid<TArg0,TArg1,TArg2,TArg3> : Continuation {
+    public delegate void Fn (TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3);
+    Fn fn; TArg0 arg0; TArg1 arg1; TArg2 arg2; TArg3 arg3;
+    public ParameterizedContinuationVoid (Fn fn, TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3) { this.fn = fn; this.arg0 = arg0; this.arg1 = arg1; this.arg2 = arg2; this.arg3 = arg3; }
+    public override void Run () { fn (arg0, arg1, arg2, arg3); }
+}
+
+public sealed class ParameterizedContinuation<TReturn,TArg0,TArg1,TArg2,TArg3,TArg4> : Continuation<TReturn> {
+    public delegate TReturn Fn (TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4);
+    Fn fn; TArg0 arg0; TArg1 arg1; TArg2 arg2; TArg3 arg3; TArg4 arg4;
+    public ParameterizedContinuation (Fn fn, TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4) { this.fn = fn; this.arg0 = arg0; this.arg1 = arg1; this.arg2 = arg2; this.arg3 = arg3; this.arg4 = arg4; }
+    public override TReturn Run () { return fn (arg0, arg1, arg2, arg3, arg4); }
+}
+
+public sealed class ParameterizedContinuationVoid<TArg0,TArg1,TArg2,TArg3,TArg4> : Continuation {
+    public delegate void Fn (TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4);
+    Fn fn; TArg0 arg0; TArg1 arg1; TArg2 arg2; TArg3 arg3; TArg4 arg4;
+    public ParameterizedContinuationVoid (Fn fn, TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4) { this.fn = fn; this.arg0 = arg0; this.arg1 = arg1; this.arg2 = arg2; this.arg3 = arg3; this.arg4 = arg4; }
+    public override void Run () { fn (arg0, arg1, arg2, arg3, arg4); }
+}
+
+public sealed class ParameterizedContinuation<TReturn,TArg0,TArg1,TArg2,TArg3,TArg4,TArg5> : Continuation<TReturn> {
+    public delegate TReturn Fn (TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5);
+    Fn fn; TArg0 arg0; TArg1 arg1; TArg2 arg2; TArg3 arg3; TArg4 arg4; TArg5 arg5;
+    public ParameterizedContinuation (Fn fn, TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5) { this.fn = fn; this.arg0 = arg0; this.arg1 = arg1; this.arg2 = arg2; this.arg3 = arg3; this.arg4 = arg4; this.arg5 = arg5; }
+    public override TReturn Run () { return fn (arg0, arg1, arg2, arg3, arg4, arg5); }
+}
+
+public sealed class ParameterizedContinuationVoid<TArg0,TArg1,TArg2,TArg3,TArg4,TArg5> : Continuation {
+    public delegate void Fn (TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5);
+    Fn fn; TArg0 arg0; TArg1 arg1; TArg2 arg2; TArg3 arg3; TArg4 arg4; TArg5 arg5;
+    public ParameterizedContinuationVoid (Fn fn, TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5) { this.fn = fn; this.arg0 = arg0; this.arg1 = arg1; this.arg2 = arg2; this.arg3 = arg3; this.arg4 = arg4; this.arg5 = arg5; }
+    public override void Run () { fn (arg0, arg1, arg2, arg3, arg4, arg5); }
+}
+
+public sealed class ParameterizedContinuation<TReturn,TArg0,TArg1,TArg2,TArg3,TArg4,TArg5,TArg6> : Continuation<TReturn> {
+    public delegate TReturn Fn (TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6);
+    Fn fn; TArg0 arg0; TArg1 arg1; TArg2 arg2; TArg3 arg3; TArg4 arg4; TArg5 arg5; TArg6 arg6;
+    public ParameterizedContinuation (Fn fn, TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6) { this.fn = fn; this.arg0 = arg0; this.arg1 = arg1; this.arg2 = arg2; this.arg3 = arg3; this.arg4 = arg4; this.arg5 = arg5; this.arg6 = arg6; }
+    public override TReturn Run () { return fn (arg0, arg1, arg2, arg3, arg4, arg5, arg6); }
+}
+
+public sealed class ParameterizedContinuationVoid<TArg0,TArg1,TArg2,TArg3,TArg4,TArg5,TArg6> : Continuation {
+    public delegate void Fn (TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6);
+    Fn fn; TArg0 arg0; TArg1 arg1; TArg2 arg2; TArg3 arg3; TArg4 arg4; TArg5 arg5; TArg6 arg6;
+    public ParameterizedContinuationVoid (Fn fn, TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6) { this.fn = fn; this.arg0 = arg0; this.arg1 = arg1; this.arg2 = arg2; this.arg3 = arg3; this.arg4 = arg4; this.arg5 = arg5; this.arg6 = arg6; }
+    public override void Run () { fn (arg0, arg1, arg2, arg3, arg4, arg5, arg6); }
+}
+
+public sealed class ParameterizedContinuation<TReturn,TArg0,TArg1,TArg2,TArg3,TArg4,TArg5,TArg6,TArg7> : Continuation<TReturn> {
+    public delegate TReturn Fn (TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7);
+    Fn fn; TArg0 arg0; TArg1 arg1; TArg2 arg2; TArg3 arg3; TArg4 arg4; TArg5 arg5; TArg6 arg6; TArg7 arg7;
+    public ParameterizedContinuation (Fn fn, TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7) { this.fn = fn; this.arg0 = arg0; this.arg1 = arg1; this.arg2 = arg2; this.arg3 = arg3; this.arg4 = arg4; this.arg5 = arg5; this.arg6 = arg6; this.arg7 = arg7; }
+    public override TReturn Run () { return fn (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7); }
+}
+
+public sealed class ParameterizedContinuationVoid<TArg0,TArg1,TArg2,TArg3,TArg4,TArg5,TArg6,TArg7> : Continuation {
+    public delegate void Fn (TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7);
+    Fn fn; TArg0 arg0; TArg1 arg1; TArg2 arg2; TArg3 arg3; TArg4 arg4; TArg5 arg5; TArg6 arg6; TArg7 arg7;
+    public ParameterizedContinuationVoid (Fn fn, TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7) { this.fn = fn; this.arg0 = arg0; this.arg1 = arg1; this.arg2 = arg2; this.arg3 = arg3; this.arg4 = arg4; this.arg5 = arg5; this.arg6 = arg6; this.arg7 = arg7; }
+    public override void Run () { fn (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7); }
+}
+
+public sealed class ParameterizedContinuation<TReturn,TArg0,TArg1,TArg2,TArg3,TArg4,TArg5,TArg6,TArg7,TArg8> : Continuation<TReturn> {
+    public delegate TReturn Fn (TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7, TArg8 arg8);
+    Fn fn; TArg0 arg0; TArg1 arg1; TArg2 arg2; TArg3 arg3; TArg4 arg4; TArg5 arg5; TArg6 arg6; TArg7 arg7; TArg8 arg8;
+    public ParameterizedContinuation (Fn fn, TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7, TArg8 arg8) { this.fn = fn; this.arg0 = arg0; this.arg1 = arg1; this.arg2 = arg2; this.arg3 = arg3; this.arg4 = arg4; this.arg5 = arg5; this.arg6 = arg6; this.arg7 = arg7; this.arg8 = arg8; }
+    public override TReturn Run () { return fn (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8); }
+}
+
+public sealed class ParameterizedContinuationVoid<TArg0,TArg1,TArg2,TArg3,TArg4,TArg5,TArg6,TArg7,TArg8> : Continuation {
+    public delegate void Fn (TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7, TArg8 arg8);
+    Fn fn; TArg0 arg0; TArg1 arg1; TArg2 arg2; TArg3 arg3; TArg4 arg4; TArg5 arg5; TArg6 arg6; TArg7 arg7; TArg8 arg8;
+    public ParameterizedContinuationVoid (Fn fn, TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7, TArg8 arg8) { this.fn = fn; this.arg0 = arg0; this.arg1 = arg1; this.arg2 = arg2; this.arg3 = arg3; this.arg4 = arg4; this.arg5 = arg5; this.arg6 = arg6; this.arg7 = arg7; this.arg8 = arg8; }
+    public override void Run () { fn (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8); }
+}
+
+public sealed class ParameterizedContinuation<TReturn,TArg0,TArg1,TArg2,TArg3,TArg4,TArg5,TArg6,TArg7,TArg8,TArg9> : Continuation<TReturn> {
+    public delegate TReturn Fn (TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7, TArg8 arg8, TArg9 arg9);
+    Fn fn; TArg0 arg0; TArg1 arg1; TArg2 arg2; TArg3 arg3; TArg4 arg4; TArg5 arg5; TArg6 arg6; TArg7 arg7; TArg8 arg8; TArg9 arg9;
+    public ParameterizedContinuation (Fn fn, TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7, TArg8 arg8, TArg9 arg9) { this.fn = fn; this.arg0 = arg0; this.arg1 = arg1; this.arg2 = arg2; this.arg3 = arg3; this.arg4 = arg4; this.arg5 = arg5; this.arg6 = arg6; this.arg7 = arg7; this.arg8 = arg8; this.arg9 = arg9; }
+    public override TReturn Run () { return fn (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9); }
+}
+
+public sealed class ParameterizedContinuationVoid<TArg0,TArg1,TArg2,TArg3,TArg4,TArg5,TArg6,TArg7,TArg8,TArg9> : Continuation {
+    public delegate void Fn (TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7, TArg8 arg8, TArg9 arg9);
+    Fn fn; TArg0 arg0; TArg1 arg1; TArg2 arg2; TArg3 arg3; TArg4 arg4; TArg5 arg5; TArg6 arg6; TArg7 arg7; TArg8 arg8; TArg9 arg9;
+    public ParameterizedContinuationVoid (Fn fn, TArg0 arg0, TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4, TArg5 arg5, TArg6 arg6, TArg7 arg7, TArg8 arg8, TArg9 arg9) { this.fn = fn; this.arg0 = arg0; this.arg1 = arg1; this.arg2 = arg2; this.arg3 = arg3; this.arg4 = arg4; this.arg5 = arg5; this.arg6 = arg6; this.arg7 = arg7; this.arg8 = arg8; this.arg9 = arg9; }
+    public override void Run () { fn (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9); }
+}
+//[[[end]]]
 }
--- src/kRPCTest/Continuations/ParameterizedContinuationTest.cs
+++ src/kRPCTest/Continuations/ParameterizedContinuationTest.cs
@@ -9,48 +9,74 @@ namespace KRPCTest.Continuations
     {
         int x;
 
-        public int FnEcho(int x)
+        public int FnEcho (double x)
         {
-            return x;
+            return (int)x;
         }
 
-        public void FnSet(int x)
+        public void FnSet (int x)
         {
             this.x = x;
         }
 
-        public void FnYield(int x)
+        public void FnYield (int x)
         {
-            throw new YieldException(new ParameterizedContinuation<int, int> (FnEcho, x));
+            throw new YieldException (new ParameterizedContinuation<int, double> (FnEcho, x));
         }
 
-        [Test]
-        public void ContinuationWithoutReturn ()
+        public void FnAdd (double x, float y)
         {
-            IContinuation cont = new ParameterizedContinuation<int,int> (FnEcho, 42);
-            Assert.AreEqual(42, cont.RunUntyped ());
+            this.x = (int)(x + y);
+        }
+
+        public int FnAddReturn (float x, double y)
+        {
+            return (int)(x + y);
         }
 
         [Test]
         public void ContinuationWithReturn ()
         {
+            IContinuation cont = new ParameterizedContinuation<int,double> (FnEcho, 42d);
+            Assert.AreEqual (42, cont.RunUntyped ());
+        }
+
+        [Test]
+        public void ContinuationWithoutReturn ()
+        {
             x = 0;
-            IContinuation cont = new ParameterizedContinuation<int> (FnSet, 42);
-            Assert.AreEqual(null, cont.RunUntyped ());
-            Assert.AreEqual(42, x);
+            IContinuation cont = new ParameterizedContinuationVoid<int> (FnSet, 42);
+            Assert.AreEqual (null, cont.RunUntyped ());
+            Assert.AreEqual (42, x);
         }
 
         [Test]
         public void ContinuationWithYield ()
         {
-            IContinuation cont = new ParameterizedContinuation<int> (FnYield, 1234);
+            IContinuation cont = new ParameterizedContinuationVoid<int> (FnYield, 1234);
             int result = 0;
             try {
                 cont.RunUntyped ();
             } catch (YieldException e) {
-                result = (int) e.Continuation.RunUntyped ();
+                result = (int)e.Continuation.RunUntyped ();
             }
-            Assert.AreEqual(1234, result);
+            Assert.AreEqual (1234, result);
+        }
+
+        [Test]
+        public void ContinuationWithoutReturnWithMultipleArguments ()
+        {
+            IContinuation cont = new ParameterizedContinuationVoid<double,float> (FnAdd, 40d, 2f);
+            Assert.AreEqual (null, cont.RunUntyped ());
+            Assert.AreEqual (42, x);
+        }
+
+        [Test]
+        public void ContinuationWithReturnWithMultipleArguments ()
+        {
+            IContinuation cont = new ParameterizedContinuation<int,float,double> (FnAddReturn, 40f, 2d);
+            Assert.AreEqual (42, cont.RunUntyped ());
+            Assert.AreEqual (42, x);
         }
     }
 }
