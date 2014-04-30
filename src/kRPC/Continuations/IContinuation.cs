92df23f57c810635c757899c7c89699934d5503c
djungelorm <djungelorm@users.noreply.github.com>
2014-04-30 11:23:48 +0100

    Clean up

--- src/kRPC/Continuations/IContinuation.cs
+++ src/kRPC/Continuations/IContinuation.cs
@@ -1,10 +1,7 @@
 namespace KRPC.Continuations
 {
-    /// <summary>
-    /// Interface for continuations. Don't inherit from this directly.
-    /// Use the type same abstract classes Continuation and Continuation<T>
-    /// </summary>
-    public interface IContinuation {
+    public interface IContinuation
+    {
         object RunUntyped ();
     };
 }
