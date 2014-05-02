af4110f9074e3203f571c6100307cd407f14ebb2
djungelorm <djungelorm@users.noreply.github.com>
2014-05-03 00:36:24 +0100

    Don't warp faster than in allowed based on altitude

--- src/kRPCSpaceCenter/Services/SpaceCenter.cs
+++ src/kRPCSpaceCenter/Services/SpaceCenter.cs
@@ -28,9 +28,9 @@ namespace KRPCSpaceCenter.Services
             var altitudeLimit = TimeWarp.fetch.GetAltitudeLimit (1, vessel.MainBody);
 
             if (vessel.Situation != VesselSituation.Landed && vessel.Situation != VesselSituation.Splashed && flight.Altitude < altitudeLimit)
-                WarpPhysicsAtRate (Mathf.Min (rate, 2));
+                WarpPhysicsAtRate (vessel, flight, Mathf.Min (rate, 2));
             else
-                WarpRegularAtRate (rate);
+                WarpRegularAtRate (vessel, flight, rate);
 
             if (rate > 1)
                 throw new YieldException (new ParameterizedContinuationVoid<double,double> (WarpTo, UT, maxRate));
@@ -38,37 +38,47 @@ namespace KRPCSpaceCenter.Services
                 TimeWarp.SetRate (0, false);
         }
 
-        static void WarpPhysicsAtRate (float rate)
+        static void WarpPhysicsAtRate (Vessel vessel, Flight flight, float rate)
         {
             throw new NotImplementedException ();
         }
 
-        static void WarpRegularAtRate (float rate)
+        static void WarpRegularAtRate (Vessel vessel, Flight flight, float rate)
         {
             if (rate < TimeWarp.fetch.warpRates [TimeWarp.CurrentRateIndex])
                 DecreaseRegularWarp ();
             if (TimeWarp.CurrentRateIndex + 1 < TimeWarp.fetch.warpRates.Length &&
                 rate > TimeWarp.fetch.warpRates [TimeWarp.CurrentRateIndex + 1])
-                IncreaseRegularWarp ();
+                IncreaseRegularWarp (vessel, flight);
+        }
+
+        static void DecreaseRegularWarp ()
+        {
+            // Check we aren't warping the minimum amount
+            if (TimeWarp.CurrentRateIndex == 0)
+                return;
+            TimeWarp.SetRate (TimeWarp.CurrentRateIndex - 1, false);
         }
 
         static double warpIncreaseAttemptTime = 0;
 
-        static void DecreaseRegularWarp ()
+        static void IncreaseRegularWarp (Vessel vessel, Flight flight)
         {
-            if (TimeWarp.CurrentRateIndex > 0)
-                TimeWarp.SetRate (TimeWarp.CurrentRateIndex - 1, false);
-        }
-
-        static void IncreaseRegularWarp ()
-        {
+            // Check if we're already warping at the maximum rate
+            if (TimeWarp.CurrentRateIndex + 1 >= TimeWarp.fetch.warpRates.Length)
+                return;
+            // Check that the previous rate update has taken effect
             float currentRate = TimeWarp.fetch.warpRates [TimeWarp.CurrentRateIndex];
-            if (TimeWarp.CurrentRateIndex + 1 < TimeWarp.fetch.warpRates.Length &&
-                (currentRate == TimeWarp.CurrentRate || currentRate <= 1) &&
-                Math.Abs (Planetarium.GetUniversalTime () - warpIncreaseAttemptTime) > 2) {
-                warpIncreaseAttemptTime = Planetarium.GetUniversalTime ();
-                TimeWarp.SetRate (TimeWarp.CurrentRateIndex + 1, false);
-            }
+            if (!(currentRate == TimeWarp.CurrentRate || currentRate <= 1))
+                return;
+            // Check we don't update the warp rate more than once every 2 seconds
+            if (Math.Abs (Planetarium.GetUniversalTime () - warpIncreaseAttemptTime) < 2)
+                return;
+            // Check we don't increase the warp rate beyond the altitude limit
+            if (flight.Altitude < TimeWarp.fetch.GetAltitudeLimit (TimeWarp.CurrentRateIndex + 1, vessel.MainBody))
+                return;
+            warpIncreaseAttemptTime = Planetarium.GetUniversalTime ();
+            TimeWarp.SetRate (TimeWarp.CurrentRateIndex + 1, false);
         }
     }
 }
