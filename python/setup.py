9aae7ce9152766c2a9f22eb778d17bf3b67ea4ca
djungelorm <djungelorm@users.noreply.github.com>
2014-05-07 14:55:50 +0000

    Update license string

--- python/setup.py
+++ python/setup.py
@@ -8,7 +8,7 @@ setup(
     packages=['krpc','krpc.schema','krpc.test'],
     scripts=['bin/example.py'],
     url='https://github.com/djungelorm/krpc',
-    license='LICENSE.txt',
+    license='GNU GPL v3',
     description='Client library for kRPC, a Remote Procedure Call server for Kerbal Space Program',
     long_description=open('README.txt').read(),
     install_requires=[
