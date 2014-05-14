2fa98a2751ace8a409fe3ad59891f9707de40ff6
djungelorm <djungelorm@users.noreply.github.com>
2014-05-14 13:26:01 +0100

    Add performance test

---
+++ python/krpc/test/test_performance.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+
+import unittest
+import subprocess
+import time
+import timeit
+import krpc
+
+class TestClient(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.server = subprocess.Popen(['bin/TestServer/TestServer.exe'])
+        time.sleep(0.25)
+
+    def setUp(self):
+        self.ksp = krpc.connect(name='TestClient')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.server.kill()
+
+    def test_basic(self):
+        def wrapper():
+            self.ksp.test_service.float_to_string(float(3.14159))
+        self.assertGreater(1, timeit.timeit(stmt=wrapper, number=1000))
+
+if __name__ == '__main__':
+    unittest.main()
