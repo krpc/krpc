c32c3a3340ece4908c12f91dabb5104875b6d570
djungelorm <djungelorm@users.noreply.github.com>
2014-05-16 14:21:30 +0100

    Add Equatable abstract class, a helper for implementing IEquatable

---
+++ src/kRPC/Utils/Equatable.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace KRPC.Utils
+{
+    public abstract class Equatable<T> : IEquatable<T>
+    {
+        public abstract bool Equals (T obj);
+        public override abstract int GetHashCode ();
+
+        public override bool Equals (object obj)
+        {
+            if (Object.ReferenceEquals(obj, null))
+                return false;
+            if (obj is T)
+                return this.Equals ((T)obj);
+            return false;
+        }
+
+        public static bool operator == (Equatable<T> lhs, Equatable<T> rhs)
+        {
+            if (Object.ReferenceEquals (lhs, rhs)) return true;
+            if (Object.ReferenceEquals (lhs, null)) return false;
+            return lhs.Equals (rhs);
+        }
+
+        public static bool operator != (Equatable<T> lhs, Equatable<T> rhs)
+        {
+            if (Object.ReferenceEquals (lhs, rhs)) return false;
+            if (Object.ReferenceEquals (lhs, null)) return false;
+            return !(lhs.Equals (rhs));
+        }
+    }
+}
