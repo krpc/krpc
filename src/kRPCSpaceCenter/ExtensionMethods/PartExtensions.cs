44e25686ce4015d7841471a60a2a1cdb45c32713
djungelorm <djungelorm@users.noreply.github.com>
2014-06-10 15:37:27 +0100

    Add Vessel.Mass and Vessel.DryMass

--- src/kRPCSpaceCenter/ExtensionMethods/PartExtensions.cs
+++ src/kRPCSpaceCenter/ExtensionMethods/PartExtensions.cs
@@ -6,6 +6,24 @@ namespace KRPCSpaceCenter.ExtensionMethods
     public static class PartExtensions
     {
         /// <summary>
+        /// Returns true if the part contains the given part module
+        /// <summary>
+        public static bool HasModule<T> (this Part part) where T : PartModule
+        {
+            return part.Modules.OfType<T> ().Any ();
+        }
+
+        /// <summary>
+        /// Returns true if the part contributes to the physics simulation (e.g. it has mass)
+        /// <summary>
+        public static bool IsPhysicallySignificant (this Part part)
+        {
+            return (!part.HasModule<ModuleLandingGear>()) &&
+                   (!part.HasModule<LaunchClamp>()) &&
+                   (part.physicalSignificance != Part.PhysicalSignificance.NONE);
+        }
+
+        /// <summary>
         /// Returns the index of the stage in which the part will be decoupled,
         /// or 0 if it is never decoupled.
         /// <summary>
@@ -19,9 +37,28 @@ namespace KRPCSpaceCenter.ExtensionMethods
             return -1;
         }
 
+        /// <summary>
+        /// Returns true if the part is a decoupler.
+        /// <summary>
         public static bool IsDecoupler (this Part part)
         {
-            return part.Modules.OfType<ModuleDecouple> ().Any () || part.Modules.OfType<ModuleAnchoredDecoupler> ().Any ();
+            return part.HasModule<ModuleDecouple> () || part.HasModule<ModuleAnchoredDecoupler> ();
+        }
+
+        /// <summary>
+        /// Returns the total mass of the part and any resources it contains.
+        /// <summary>
+        public static float TotalMass (this Part part)
+        {
+            return part.mass + part.GetResourceMass ();
+        }
+
+        /// <summary>
+        /// Returns the total mass of the part, excluding any resources it contains.
+        /// <summary>
+        public static float DryMass (this Part part)
+        {
+            return part.mass;
         }
     }
 }
