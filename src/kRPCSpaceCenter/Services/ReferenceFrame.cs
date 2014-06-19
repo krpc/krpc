c883c0070aa8c024590c8cbd40bed25f9cac147c
djungelorm <djungelorm@users.noreply.github.com>
2014-06-19 21:45:14 +0100

    Clean up

--- src/kRPCSpaceCenter/Services/ReferenceFrame.cs
+++ src/kRPCSpaceCenter/Services/ReferenceFrame.cs
@@ -64,6 +64,100 @@ namespace KRPCSpaceCenter.Services
         }
 
         /// <summary>
+        /// Returns the position of the origin of the reference frame in world-space.
+        /// </summary>
+        public Vector3d Position {
+            get {
+                switch (type) {
+                case Type.CelestialBody:
+                    return body.position;
+                case Type.Vessel:
+                    return vessel.GetWorldPos3D ();
+                case Type.CelestialBodySurface:
+                case Type.Orbital:
+                case Type.Maneuver:
+                case Type.Part:
+                    throw new NotImplementedException ();
+                default:
+                    throw new ArgumentException ("No such reference frame");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the velocity of the reference frame in world-space.
+        /// </summary>
+        public Vector3d Velocity {
+            get {
+                switch (type) {
+                case Type.CelestialBody:
+                    {
+                        // TODO: better way to check for having an orbit, that doesn't depend on name?
+                        if (body.name != "Sun") {
+                            return body.GetOrbit ().GetVel ();
+                        } else {
+                            // Get a body that orbits the sun
+                            var orbitingBody = FlightGlobals.Bodies.Find (b => b.name != "Sun" && b.GetOrbit ().referenceBody == body);
+                            var orbit = orbitingBody.GetOrbit ();
+                            // Compute the velocity of the sun in world space from this body
+                            // Can't be done for from the sun object as it has no orbit object
+                            return orbit.GetVel () - orbit.GetRelativeVel ();
+                        }
+                    }
+                case Type.CelestialBodySurface:
+                    {
+                        if (vessel.mainBody != body)
+                            throw new ArgumentException ("Vessel is in orbit around a different body");
+                        return ((Vector3d)vessel.GetObtVelocity ()) - ((Vector3d)vessel.GetSrfVelocity ());
+                    }
+                case Type.Vessel:
+                    return vessel.GetOrbit ().GetVel ();
+                case Type.Orbital:
+                    // TODO: is this correct? (relative to world space)
+                    return Vector3d.zero;
+                case Type.Maneuver:
+                case Type.Part:
+                    throw new NotImplementedException ();
+                default:
+                    throw new ArgumentException ("No such reference frame");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the rotation of the given frame of reference, relative to world space.
+        /// Applying the rotation to a vector in reference-frame-space produces the corresponding vector in world-space.
+        /// </summary>
+        public QuaternionD Rotation {
+            get {
+                Vector3d forward = ForwardNotNormalized;
+                // Note: forward is along the z-axis, up is along the negative y-axis
+                Vector3d up = -UpNotNormalized;
+                //FIXME: Vector3d.OrthoNormalize and QuaternionD.LookRotation methods are not found at run-time
+                //Vector3d.OrthoNormalize (ref forward, ref up);
+                //return QuaternionD.LookRotation (forward, up);
+                Vector3 forward2 = forward;
+                Vector3 up2 = up;
+                Vector3.OrthoNormalize (ref forward2, ref up2);
+                return Quaternion.LookRotation (forward2, up2);
+            }
+        }
+
+        /// <summary>
+        /// Returns the up vector of the reference frame in world coordinates.
+        /// </summary>
+        public Vector3d Up {
+            get { return UpNotNormalized.normalized; }
+        }
+
+        /// <summary>
+        /// Returns the forward vector of the reference frame in world coordinates.
+        /// </summary>
+        public Vector3d Forward {
+            get { return ForwardNotNormalized.normalized; }
+        }
+
+        /// <summary>
         /// Returns the up vector for the reference frame in world coordinates.
         /// The vector is not normalized.
         /// </summary>
@@ -117,100 +211,6 @@ namespace KRPCSpaceCenter.Services
                         } else
                             throw new NotImplementedException ();
                     }
-                default:
-                    throw new ArgumentException ("No such reference frame");
-                }
-            }
-        }
-
-        /// <summary>
-        /// Returns the up vector of the reference frame in world coordinates.
-        /// </summary>
-        public Vector3d Up {
-            get { return UpNotNormalized.normalized; }
-        }
-
-        /// <summary>
-        /// Returns the forward vector of the reference frame in world coordinates.
-        /// </summary>
-        public Vector3d Forward {
-            get { return ForwardNotNormalized.normalized; }
-        }
-
-        /// <summary>
-        /// Returns the rotation of the given frame of reference, relative to world space.
-        /// Applying the rotation to a vector in reference-frame-space produces the corresponding vector in world-space.
-        /// </summary>
-        public QuaternionD Rotation {
-            get {
-                Vector3d forward = ForwardNotNormalized;
-                // Note: forward is along the z-axis, up is along the negative y-axis
-                Vector3d up = -UpNotNormalized;
-                //FIXME: Vector3d.OrthoNormalize and QuaternionD.LookRotation methods are not found at run-time
-                //Vector3d.OrthoNormalize (ref forward, ref up);
-                //return QuaternionD.LookRotation (forward, up);
-                Vector3 forward2 = forward;
-                Vector3 up2 = up;
-                Vector3.OrthoNormalize (ref forward2, ref up2);
-                return Quaternion.LookRotation (forward2, up2);
-            }
-        }
-
-        /// <summary>
-        /// Returns the velocity of the reference frame in world-space.
-        /// </summary>
-        public Vector3d Velocity {
-            get {
-                switch (type) {
-                case Type.CelestialBody:
-                    {
-                        // TODO: better way to check for having an orbit, that doesn't depend on name?
-                        if (body.name != "Sun") {
-                            return body.GetOrbit ().GetVel ();
-                        } else {
-                            // Get a body that orbits the sun
-                            var orbitingBody = FlightGlobals.Bodies.Find (b => b.name != "Sun" && b.GetOrbit ().referenceBody == body);
-                            var orbit = orbitingBody.GetOrbit ();
-                            // Compute the velocity of the sun in world space from this body
-                            // Can't be done for from the sun object as it has no orbit object
-                            return orbit.GetVel () - orbit.GetRelativeVel ();
-                        }
-                    }
-                case Type.CelestialBodySurface:
-                    {
-                        if (vessel.mainBody != body)
-                            throw new ArgumentException ("Vessel is in orbit around a different body");
-                        return ((Vector3d)vessel.GetObtVelocity ()) - ((Vector3d)vessel.GetSrfVelocity ());
-                    }
-                case Type.Vessel:
-                    return vessel.GetOrbit ().GetVel ();
-                case Type.Orbital:
-                    // TODO: is this correct? (relative to world space)
-                    return Vector3d.zero;
-                case Type.Maneuver:
-                case Type.Part:
-                    throw new NotImplementedException ();
-                default:
-                    throw new ArgumentException ("No such reference frame");
-                }
-            }
-        }
-
-        /// <summary>
-        /// Returns the position of the origin of the reference frame in world-space.
-        /// </summary>
-        public Vector3d Position {
-            get {
-                switch (type) {
-                case Type.CelestialBody:
-                    return body.position;
-                case Type.Vessel:
-                    return vessel.GetWorldPos3D ();
-                case Type.CelestialBodySurface:
-                case Type.Orbital:
-                case Type.Maneuver:
-                case Type.Part:
-                    throw new NotImplementedException ();
                 default:
                     throw new ArgumentException ("No such reference frame");
                 }
