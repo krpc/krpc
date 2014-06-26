07929a985a1b6d7996291340eb4d9f52a95c8995
djungelorm <djungelorm@users.noreply.github.com>
2014-06-26 21:50:36 +0100

    Add cross product

--- test/kRPCSpaceCenterTest/mathtools.py
+++ test/kRPCSpaceCenterTest/mathtools.py
@@ -16,6 +16,11 @@ def normalize(v):
 def dot(u,v):
     return sum(x*y for x,y in itertools.izip(u,v))
 
+def cross(u, v):
+    return tuple([u[1]*v[2] - u[2]*v[1],
+                  u[2]*v[0] - u[0]*v[2],
+                  u[0]*v[1] - u[1]*v[0]])
+
 @functools.total_ordering
 class vector(object):
     def __init__(self, v):
