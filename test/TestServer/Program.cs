f7813f8d8d81fbaa95f23080f7311aa2f539f564
djungelorm <djungelorm@users.noreply.github.com>
2014-07-08 14:19:16 +0100

    Improve performance of main server loop (#68)

--- test/TestServer/Program.cs
+++ test/TestServer/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net;
+using System.Diagnostics;
 using KRPC;
 
 namespace TestServer
@@ -8,14 +9,21 @@ namespace TestServer
     {
         public static void Main (string[] args)
         {
+            var frameTime = 50;
             var server = new KRPCServer (IPAddress.Loopback, ushort.Parse (args [0]));
             var timeSpan = new TimeSpan ();
             server.GetUniversalTime = () => timeSpan.TotalSeconds;
             server.OnClientRequestingConnection += (s, e) => e.Request.Allow ();
             server.Start ();
             Console.WriteLine ("Started test server...");
-            while (server.Running)
+            while (server.Running) {
+                Stopwatch timer = Stopwatch.StartNew ();
                 server.Update ();
+                var elapsed = timer.ElapsedMilliseconds;
+                var sleep = frameTime - elapsed;
+                if (sleep > 0)
+                    System.Threading.Thread.Sleep ((int)sleep);
+            }
             Console.WriteLine ("Test server stopped");
         }
     }
