d80b1db12966d41a59f2857b41d744f3d628938e
djungelorm <djungelorm@users.noreply.github.com>
2014-07-10 00:50:37 +0100

    Improve testingtools

--- test/kRPCSpaceCenterTest/testingtools.py
+++ test/kRPCSpaceCenterTest/testingtools.py
@@ -27,26 +27,48 @@ def load_save(name):
 
 class TestCase(unittest.TestCase):
 
-    def assertBetween(self, min_value, max_value, value):
-        if min_value < value and value < max_value:
-            return
-        self.fail('%f is not between %f and %f' % (value,min_value,max_value))
+    def _isInRange(self, min_value, max_value, value):
+        return min_value <= value and value <= max_value
 
-    def assertNotBetween(self, min_value, max_value, value):
-        if value < min_value or max_value < value:
-            return
-        self.fail('%f is between %f and %f' % (value,min_value,max_value))
-
-    def assertClose(self, expected, actual, error=0.001):
+    def _isClose(self, expected, actual, error=0.001):
         if type(expected) in (list,tuple):
-            for x,y in itertools.izip(expected, actual):
-                if len(expected) != len(actual):
-                    self.fail(str(actual) + ' is not close to ' + str(expected))
-                self.assertBetween(x-error, x+error, y)
-        else:
-            self.assertBetween(expected-error, expected+error, actual)
+            ok = True
+            if len(expected) != len(actual):
+                ok = False
+            else:
+                for x,y in itertools.izip(expected, actual):
+                    if not self._isInRange(x-error, x+error, y):
+                        ok = False
+                        break
+            if not ok:
+                return False
+        elif not self._isInRange(expected-error, expected+error, actual):
+            return False
+        return True
+
+    def assertNotClose(self, expected, actual, error=0.01):
+        """ Check that actual is not equal to expected, within the given absolute error
+            i.e. actual is not in the range (expected-error,expected+error) """
+        if self._isClose(expected, actual, error):
+            if type(expected) in (list,tuple):
+                args = [str(tuple(x)) for x in (actual,expected)] + [error]
+                self.fail('%s is close to %s, within an absolute error of %f' % tuple(args))
+            else:
+                self.fail('%f is close to %f, within an absolute error of %f' % (actual,expected,error))
+
+    def assertClose(self, expected, actual, error=0.01):
+        """ Check that actual is equal to expected, within the given absolute error
+            i.e. actual is in the range (expected-error,expected+error) """
+        if not self._isClose(expected, actual, error):
+            if type(expected) in (list,tuple):
+                args = [str(tuple(x)) for x in (actual,expected)] + [error]
+                self.fail('%s is not close to %s, within an absolute error of %f' % tuple(args))
+            else:
+                self.fail('%f is not close to %f, within an absolute error of %f' % (actual,expected,error))
 
     def assertCloseDegrees(self, expected, actual, error=0.001):
+        """ Check that angle actual is equal to angle expected, within the given absolute error.
+            Uses clock arithmetic to compare angles, in range (0,360] """
         def _clamp_degrees(a):
             a = a % 360
             if a < 0:
@@ -56,11 +78,11 @@ class TestCase(unittest.TestCase):
         min, max = _clamp_degrees(expected - error), _clamp_degrees(expected + error)
         actual = _clamp_degrees(actual)
 
-        if min < actual and actual < max:
+        if min <= actual and actual <= max:
             return
-        if (min > max) and (0 <= actual) and (actual < max):
+        if (min > max) and (0 <= actual) and (actual <= max):
             return
-        if (min > max) and (min < actual) and (actual <= 360):
+        if (min > max) and (min <= actual) and (actual <= 360):
             return
 
-        self.fail('Angle %.2f is not close to %.2f' % (actual, expected))
+        self.fail('Angle %.2f is not close to %.2f, within an absolute error of %f' % (actual, expected, error))
