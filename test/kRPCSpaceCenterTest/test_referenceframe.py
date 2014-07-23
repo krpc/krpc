540b59d61c3c0d9e914542a6fd422f2d499cf8e9
djungelorm <djungelorm@users.noreply.github.com>
2014-07-23 11:22:46 +0100

    Add velocity and direction tests

--- test/kRPCSpaceCenterTest/test_referenceframe.py
+++ test/kRPCSpaceCenterTest/test_referenceframe.py
@@ -120,5 +120,118 @@ class TestBody(testingtools.TestCase):
         # TODO: large error
         self.assertClose((0,0,0), p, error=50)
 
+    def check_object_velocity(self, obj, ref):
+        # Check velocity vectors are unchanged by converting between the same reference frame
+        self.assertClose((1,2,3), self.sc.transform_velocity((0,0,0), (1,2,3), ref, ref))
+        if obj.orbit is not None:
+            # Check velocity of reference frame is same as orbital speed in reference frame of body being orbited
+            v = self.sc.transform_velocity((0,0,0), (0,0,0), ref, obj.orbit.body.non_rotating_reference_frame)
+            self.assertClose(norm(v), obj.orbit.speed, error=0.5)
+
+    def check_object_surface_velocity(self, obj, ref):
+        if obj.orbit is not None:
+            # Check rotational component of velocity same as orbital speed
+            v = self.sc.transform_velocity((0,0,0), (0,0,0), ref, obj.orbit.body.reference_frame)
+            if obj.orbit.inclination == 0:
+                self.assertClose(0, v[1])
+            else:
+                self.assertNotClose(0, v[1])
+            angular_velocity = obj.orbit.body.angular_velocity(obj.orbit.body.non_rotating_reference_frame)
+            self.assertClose(0, angular_velocity[0])
+            self.assertClose(0, angular_velocity[2])
+            rotational_speed = dot((0,1,0), angular_velocity)
+            position = list(obj.position(obj.orbit.body.reference_frame))
+            position[1] = 0
+            radius = norm(position)
+            rotational_speed *= radius
+            #TODO: large error
+            self.assertClose(abs(rotational_speed + obj.orbit.speed), norm(v), error=200)
+
+    def test_celestial_body_velocity(self):
+        for body in self.bodies.values():
+            self.check_object_velocity(body, body.reference_frame)
+            self.check_object_surface_velocity(body, body.reference_frame)
+
+    def test_celestial_body_orbital_velocity(self):
+        for body in self.bodies.values():
+            if body.orbit is not None:
+                self.check_object_velocity(body, body.orbital_reference_frame)
+                self.check_object_surface_velocity(body, body.orbital_reference_frame)
+
+    def test_celestial_body_surface_velocity(self):
+        for body in self.bodies.values():
+            if body.orbit is not None:
+                self.check_object_surface_velocity(body, body.surface_reference_frame)
+                self.check_object_surface_velocity(body, body.surface_reference_frame)
+
+    def test_vessel_velocity(self):
+        self.check_object_velocity(self.vessel, self.vessel.reference_frame)
+        self.check_object_surface_velocity(self.vessel, self.vessel.reference_frame)
+
+    def test_vessel_orbital_velocity(self):
+        self.check_object_velocity(self.vessel, self.vessel.orbital_reference_frame)
+        self.check_object_surface_velocity(self.vessel, self.vessel.orbital_reference_frame)
+
+    def test_vessel_surface_velocity(self):
+        self.check_object_velocity(self.vessel, self.vessel.surface_reference_frame)
+        self.check_object_surface_velocity(self.vessel, self.vessel.surface_reference_frame)
+
+    def test_part_velocity(self):
+        # TODO: implement
+        pass
+
+    def test_part_orbital_velocity(self):
+        # TODO: implement
+        pass
+
+    def test_part_surface_velocity(self):
+        # TODO: implement
+        pass
+
+    def test_maneuver_velocity(self):
+        # TODO: implement
+        pass
+
+    def test_celestial_body_direction(self):
+        # Check (0,1,0) direction same as body direction
+        for body in self.bodies.values():
+            self.assertClose((0,1,0), body.direction(body.reference_frame))
+
+    def test_celestial_body_orbital_direction(self):
+        # TODO: implement
+        pass
+
+    def test_celestial_body_surface_direction(self):
+        # TODO: implement
+        pass
+
+    def test_vessel_direction(self):
+        # Check (0,1,0) direction same as vessel direction
+        self.assertClose((0,1,0), self.vessel.direction(self.vessel.reference_frame))
+
+    def test_vessel_orbital_direction(self):
+        # TODO: implement
+        pass
+
+    def test_vessel_surface_direction(self):
+        # TODO: implement
+        pass
+
+    def test_part_direction(self):
+        # TODO: implement
+        pass
+
+    def test_part_orbital_direction(self):
+        # TODO: implement
+        pass
+
+    def test_part_surface_direction(self):
+        # TODO: implement
+        pass
+
+    def test_maneuver_direction(self):
+        # TODO: implement
+        pass
+
 if __name__ == "__main__":
     unittest.main()
