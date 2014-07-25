ac924032534bb5ac62cd37a3655b10c7afd63ac4
djungelorm <djungelorm@users.noreply.github.com>
2014-07-25 11:37:11 +0100

    Refactor orbit tests

--- test/kRPCSpaceCenterTest/test_referenceframe.py
+++ test/kRPCSpaceCenterTest/test_referenceframe.py
@@ -2,6 +2,7 @@ import unittest
 import testingtools
 from testingtools import load_save
 from mathtools import *
+from geometrytools import compute_position
 import krpc
 import time
 import itertools
@@ -32,48 +33,9 @@ class TestBody(testingtools.TestCase):
         # Check position agrees with that calculated from bodies orbit
         if obj.name in ('Kerbin','Mun','Minmus','Test'):
             ref = obj.orbit.body.reference_frame
-            expected_pos = self.compute_position(obj, ref)
+            expected_pos = compute_position(obj, ref)
             actual_pos = tuple(x / 1000000 for x in obj.position(ref))
             self.assertClose(expected_pos, actual_pos, error=1)
-
-    def compute_position(self, obj, ref):
-        """ Compute the objects position in the given reference frame (in Mm) from it's orbital elements """
-        orbit = obj.orbit
-
-        major_axis = orbit.semi_major_axis / 1000000
-        minor_axis = orbit.semi_minor_axis / 1000000
-
-        eccentricity = orbit.eccentricity
-        mean_anomaly = orbit.mean_anomaly
-        eccentric_anomaly = orbit.eccentric_anomaly
-
-        x = major_axis * math.cos(eccentric_anomaly)
-        z = minor_axis * math.sin(eccentric_anomaly)
-        pos = (x,0,z)
-        pos_magnitude = norm(pos)
-        pos_direction = normalize(pos)
-        self.assertClose(1, norm(pos_direction))
-
-        angle = orbit.argument_of_periapsis
-        rotation = quaternion_axis_angle((0,1,0), -angle)
-        pos = quaternion_vector_mult(rotation, pos)
-
-        angle = orbit.inclination
-        rotation = quaternion_axis_angle((1,0,0), -angle)
-        pos = quaternion_vector_mult(rotation, pos)
-
-        angle = orbit.longitude_of_ascending_node
-        rotation = quaternion_axis_angle((0,1,0), -angle)
-        pos = quaternion_vector_mult(rotation, pos)
-
-        reference_normal = orbit.reference_plane_normal(ref)
-        self.assertClose((0,1,0), reference_normal)
-        reference_direction = orbit.reference_plane_direction(ref)
-        reference_angle = math.acos(dot((1,0,0),reference_direction))
-        reference_rotation = quaternion_axis_angle((0,1,0), reference_angle)
-        x_rotated = quaternion_vector_mult(reference_rotation, (1,0,0))
-        self.assertClose(x_rotated, reference_direction)
-        return quaternion_vector_mult(reference_rotation, pos)
 
     def test_celestial_body_position(self):
         for body in self.bodies.values():
