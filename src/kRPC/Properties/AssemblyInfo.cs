b5f5200beb08c0a7bde66b4331c6258c5706e82a
djungelorm <djungelorm@users.noreply.github.com>
2015-03-09 18:23:25 +0000

    Bump version to 0.1.4

--- src/kRPC/Properties/AssemblyInfo.cs
+++ src/kRPC/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright ("djungelorm")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
-[assembly: AssemblyVersion ("0.1.3")]
+[assembly: AssemblyVersion ("0.1.4")]
 [assembly: InternalsVisibleTo ("kRPCTest")]
 [assembly: InternalsVisibleTo ("TestServer")]
 [assembly: InternalsVisibleTo ("DynamicProxyGenAssembly2")]
