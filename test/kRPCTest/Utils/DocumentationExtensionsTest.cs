4c14eabf49fe09570446bcd088e39157d5056a44
djungelorm <djungelorm@users.noreply.github.com>
2015-03-10 01:17:32 +0000

    Add documentation extension methods test

---
+++ test/kRPCTest/Utils/DocumentationExtensionsTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using KRPC.Utils;
+using NUnit.Framework;
+
+namespace KRPCTest.Utils
+{
+    [TestFixture]
+    public class DocumentationExtentionsTest
+    {
+        /// <summary>
+        /// Baz
+        /// </summary>
+        void foo ()
+        {
+        }
+
+        void bar ()
+        {
+        }
+
+        [Test]
+        public void TestLoadDocumentation ()
+        {
+            var method = typeof(DocumentationExtentionsTest).GetMethod ("foo", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            Assert.AreEqual ("Baz", method.GetDocumentation ());
+        }
+
+        [Test]
+        public void TestNoDocumentation ()
+        {
+            var method = typeof(DocumentationExtentionsTest).GetMethod ("bar", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            Assert.AreEqual ("", method.GetDocumentation ());
+        }
+    }
+}
