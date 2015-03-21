c152b42bf150292018d0910d833c4e426405f053
djungelorm <djungelorm@users.noreply.github.com>
2015-03-21 23:45:28 +0000

    Draw directions from correct origin

--- src/kRPCSpaceCenter/DrawAddon.cs
+++ src/kRPCSpaceCenter/DrawAddon.cs
@@ -55,9 +55,10 @@ namespace KRPCSpaceCenter
             // Render directions on the active vessel
             var vessel = FlightGlobals.ActiveVessel;
             if (vessel != null) {
+                var origin = vessel.GetWorldPos3D ();
                 foreach (var direction in directions) {
-                    direction.Item3.SetPosition (0, vessel.CoM);
-                    direction.Item3.SetPosition (1, vessel.CoM + (direction.Item6.DirectionToWorldSpace (direction.Item4) * direction.Item5));
+                    direction.Item3.SetPosition (0, origin);
+                    direction.Item3.SetPosition (1, origin + (direction.Item6.DirectionToWorldSpace (direction.Item4) * direction.Item5));
                 }
             }
         }
