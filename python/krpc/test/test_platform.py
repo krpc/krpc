166de2f0c38a14d1f61c7fc79f5b9b25569f29d9
djungelorm <djungelorm@users.noreply.github.com>
2015-04-13 00:43:25 +0100

    Add krpc.platform tests

---
+++ python/krpc/test/test_platform.py
@@ -0,0 +1,23 @@
+import unittest
+from krpc.platform import bytelength, hexlify, unhexlify
+
+class TestPlatform(unittest.TestCase):
+
+    def test_bytelength(self):
+        self.assertEqual(0, bytelength(''))
+        self.assertEqual(3, bytelength('foo'))
+        self.assertEqual(3, bytelength(b'\xe2\x84\xa2'.decode('utf-8')))
+        self.assertEqual(3, bytelength(u'\u2122'))
+
+    def test_hexlify(self):
+        self.assertEqual(hexlify(b''), '')
+        self.assertEqual(hexlify(b'\x00\x01\x02'), '000102')
+        self.assertEqual(hexlify(b'\xFF'), 'ff')
+
+    def test_unhexlify(self):
+        self.assertEqual(unhexlify(''), b'')
+        self.assertEqual(unhexlify('000102'), b'\x00\x01\x02')
+        self.assertEqual(unhexlify('ff'), b'\xFF')
+
+if __name__ == '__main__':
+    unittest.main()
