87af31702fc255f03125db74710930603459c037
djungelorm <djungelorm@users.noreply.github.com>
2015-05-14 01:28:45 +0100

    Update set-version script

--- tools/set-version.sh
+++ tools/set-version.sh
@@ -9,6 +9,10 @@ sed -i -E 's/AssemblyVersion \(".+"\)/AssemblyVersion \("'$VERSION'\")/' \
 sed -i -E 's/AssemblyVersion \(".+"\)/AssemblyVersion \("'$VERSION'\")/' \
        src/kRPCSpaceCenter/Properties/AssemblyInfo.cs
 sed -i -E 's/AssemblyVersion \(".+"\)/AssemblyVersion \("'$VERSION'\")/' \
+       src/kRPCInfernalRobotics/Properties/AssemblyInfo.cs
+sed -i -E 's/AssemblyVersion \(".+"\)/AssemblyVersion \("'$VERSION'\")/' \
+       src/kRPCKerbalAlarmClock/Properties/AssemblyInfo.cs
+sed -i -E 's/AssemblyVersion \(".+"\)/AssemblyVersion \("'$VERSION'\")/' \
        test/TestServer/Properties/AssemblyInfo.cs
 sed -i -E "s/version='.+'/version='"$VERSION"'/" python/setup.py
 sed -i -E "s/release = '.+'/release = '"$VERSION"'/" doc/src/conf.py
