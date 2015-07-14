4760cb82c81d55f133cafb4200939685d2d267be
djungelorm <djungelorm@users.noreply.github.com>
2015-07-14 20:42:13 +0100

    Add script to download and save the results of a KRPC.GetServices() call

---
+++ doc/get-api.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import json
+import krpc
+from krpc.types import Types
+from krpc.decoder import Decoder
+
+conn = krpc.connect(name='get-api.py')
+types = Types()
+
+info = {}
+for service in conn.krpc.get_services().services:
+    service_info = {
+        'procedures': {},
+        'classes': [],
+        'enumerations': {},
+        'documentation': service.documentation
+    }
+    info[service.name] = service_info
+    for procedure in service.procedures:
+        procedure_info = {
+            'parameters': [],
+            'return_type': procedure.return_type,
+            'attributes': [str(x) for x in procedure.attributes],
+            'documentation': procedure.documentation
+        }
+        service_info['procedures'][procedure.name] = procedure_info
+        for parameter in procedure.parameters:
+
+            default_argument = None
+            if parameter.HasField('default_argument'):
+                typ = types.as_type(parameter.type)
+                default_argument = Decoder.decode(parameter.default_argument, typ)
+
+            parameter_info = {
+                'name': parameter.name,
+                'type': parameter.type,
+                'default_argument': default_argument
+            }
+            procedure_info['parameters'].append(parameter_info)
+    for cls in service.classes:
+        service_info['classes'].append(cls.name)
+    for enum in service.enumerations:
+        enum_info = []
+        service_info['enumerations'][enum.name] = enum_info
+        for value in enum.values:
+            enum_info.append({'name': value.name, 'value': value.value})
+
+print json.dumps(info, sort_keys=True, indent=4, separators=(',', ': '))
