8bda5ebcf09e7e36a422fd2408e2d69a54483563
djungelorm <djungelorm@users.noreply.github.com>
2016-01-22 10:31:51 -0800

    Add checks to ServiceDefinitions tool

--- tools/ServiceDefinitions/Program.cs
+++ tools/ServiceDefinitions/Program.cs
@@ -3,28 +3,34 @@ using Newtonsoft.Json;
 using System.Linq;
 using System.Reflection;
 using System.IO;
+using System.Collections.Generic;
 using KRPC.Utils;
 
 namespace ServiceDefinitions
 {
     class MainClass
     {
-        public static void Main (string[] args)
+        public static int Main (string[] args)
         {
+            if (args.Length < 2) {
+                Console.Error.WriteLine ("Not enough arguments.\nUsage: ServiceDefinitions.exe SERVICE OUTPUT");
+                return 1;
+            }
             Logger.Enabled = true;
             Logger.Level = Logger.Severity.Warning;
             var service = args [0];
             var outputPath = args [1];
             for (var i = 2; i < args.Count (); i++)
-                Assembly.LoadFrom (args[i]);
+                Assembly.LoadFrom (args [i]);
             var services = KRPC.Service.Scanner.Scanner.GetServices ();
-            foreach (var key in services.Keys.ToList()) {
-                if (key != service) {
-                    services.Remove (key);
-                }
+            if (!services.ContainsKey (service)) {
+                Console.Error.WriteLine ("Service " + service + " not found");
+                return 1;
             }
+            services = new Dictionary<string,KRPC.Service.Scanner.ServiceSignature> { { service, services [service] } };
             string output = JsonConvert.SerializeObject (services, Formatting.Indented);
             File.WriteAllText (outputPath, output);
+            return 0;
         }
     }
 }
