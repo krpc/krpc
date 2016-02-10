9c6029b74473b74d03bcc0515f80cc1aeb520f14
djungelorm <djungelorm@users.noreply.github.com>
2016-02-09 22:28:12 -0800

    Improve error checking in ServiceDefinitions tool

--- tools/ServiceDefinitions/Program.cs
+++ tools/ServiceDefinitions/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.IO;
 using System.Collections.Generic;
+using System.Security;
 using KRPC.Utils;
 
 namespace ServiceDefinitions
@@ -20,8 +21,40 @@ namespace ServiceDefinitions
             Logger.Level = Logger.Severity.Warning;
             var service = args [0];
             var outputPath = args [1];
-            for (var i = 2; i < args.Count (); i++)
-                Assembly.LoadFrom (args [i]);
+            for (var i = 2; i < args.Count (); i++) {
+                var path = args[i];
+                try {
+                    Assembly.LoadFrom (path);
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.Error.WriteLine ("Assembly '"+ path +"' not found.");
+                    return 1;
+                }
+                catch (FileLoadException e)
+                {
+                    Console.Error.WriteLine ("Failed to load assembly '"+ path +"'.");
+                    Console.Error.WriteLine (e.Message);
+                    if (e.InnerException != null)
+                        Console.Error.WriteLine (e.InnerException.Message);
+                    return 1;
+                }
+                catch (BadImageFormatException)
+                {
+                    Console.Error.WriteLine ("Failed to load assembly '"+ path +"'. Bad image format.");
+                    return 1;
+                }
+                catch (SecurityException)
+                {
+                    Console.Error.WriteLine ("Failed to load assembly '"+ path +"'. Security exception.");
+                    return 1;
+                }
+                catch (PathTooLongException)
+                {
+                    Console.Error.WriteLine ("Failed to load assembly '"+ path +"'. File name too long.");
+                    return 1;
+                }
+            }
             var services = KRPC.Service.Scanner.Scanner.GetServices ();
             if (!services.ContainsKey (service)) {
                 Console.Error.WriteLine ("Service " + service + " not found");
