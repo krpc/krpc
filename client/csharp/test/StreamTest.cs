a9463e91fdcf858eb594db4a86af74cb8c615c9e
djungelorm <djungelorm@users.noreply.github.com>
2016-02-17 21:08:28 -0800

    Improve stream tests

--- client/csharp/test/StreamTest.cs
+++ client/csharp/test/StreamTest.cs
@@ -19,12 +19,12 @@ namespace KRPC.Client.Test
         public void Init ()
         {
             // FIXME: there is a race condition in the server, if we call add stream too soon!
-            Thread.Sleep (100);
+            Thread.Sleep (50);
         }
 
         void Wait ()
         {
-            Thread.Sleep (25);
+            Thread.Sleep (50);
         }
 
         [Test]
@@ -32,8 +32,8 @@ namespace KRPC.Client.Test
         {
             var x = connection.AddStream (() => connection.TestService ().FloatToString (3.14159f));
             for (int i = 0; i < 5; i++) {
-                Wait ();
                 Assert.AreEqual ("3.14159", x.Get ());
+                Wait ();
             }
         }
 
@@ -43,8 +43,8 @@ namespace KRPC.Client.Test
             connection.TestService ().StringProperty = "foo";
             var x = connection.AddStream (() => connection.TestService ().StringProperty);
             for (int i = 0; i < 5; i++) {
-                Wait ();
                 Assert.AreEqual ("foo", x.Get ());
+                Wait ();
             }
         }
 
@@ -54,8 +54,8 @@ namespace KRPC.Client.Test
             var obj = connection.TestService ().CreateTestObject ("bob");
             var x = connection.AddStream (() => obj.FloatToString (3.14159f));
             for (int i = 0; i < 5; i++) {
-                Wait ();
                 Assert.AreEqual ("bob3.14159", x.Get ());
+                Wait ();
             }
         }
 
@@ -65,8 +65,8 @@ namespace KRPC.Client.Test
             // FIXME: have to specify optional parameter ""
             var x = connection.AddStream (() => TestClass.StaticMethod (connection, "foo", ""));
             for (int i = 0; i < 5; i++) {
-                Wait ();
                 Assert.AreEqual ("jebfoo", x.Get ());
+                Wait ();
             }
         }
 
@@ -77,20 +77,20 @@ namespace KRPC.Client.Test
             obj.IntProperty = 42;
             var x = connection.AddStream (() => obj.IntProperty);
             for (int i = 0; i < 5; i++) {
-                Wait ();
                 Assert.AreEqual (42, x.Get ());
+                Wait ();
             }
         }
 
         [Test]
         public void Counter ()
         {
-            var count = 0;
+            var count = -1;
             var x = connection.AddStream (() => connection.TestService ().Counter ());
             for (int i = 0; i < 5; i++) {
-                Wait ();
                 Assert.IsTrue (count < x.Get ());
                 count = x.Get ();
+                Wait ();
             }
         }
 
@@ -100,9 +100,9 @@ namespace KRPC.Client.Test
             var x0 = connection.AddStream (() => connection.TestService ().FloatToString (0.123f));
             var x1 = connection.AddStream (() => connection.TestService ().FloatToString (1.234f));
             for (int i = 0; i < 5; i++) {
-                Wait ();
                 Assert.AreEqual ("0.123", x0.Get ());
                 Assert.AreEqual ("1.234", x1.Get ());
+                Wait ();
             }
         }
 
@@ -110,32 +110,52 @@ namespace KRPC.Client.Test
         public void Inerleaved ()
         {
             var s0 = connection.AddStream (() => connection.TestService ().Int32ToString (0));
+            Assert.AreEqual ("0", s0.Get ());
+
             Wait ();
             Assert.AreEqual ("0", s0.Get ());
 
             var s1 = connection.AddStream (() => connection.TestService ().Int32ToString (1));
+            Assert.AreEqual ("0", s0.Get ());
+            Assert.AreEqual ("1", s1.Get ());
+
             Wait ();
             Assert.AreEqual ("0", s0.Get ());
             Assert.AreEqual ("1", s1.Get ());
 
             s1.Remove ();
+            Assert.AreEqual ("0", s0.Get ());
+            Assert.Throws<InvalidOperationException> (() => s1.Get ());
+
             Wait ();
             Assert.AreEqual ("0", s0.Get ());
             Assert.Throws<InvalidOperationException> (() => s1.Get ());
 
             var s2 = connection.AddStream (() => connection.TestService ().Int32ToString (2));
+            Assert.AreEqual ("0", s0.Get ());
+            Assert.Throws<InvalidOperationException> (() => s1.Get ());
+            Assert.AreEqual ("2", s2.Get ());
+
             Wait ();
             Assert.AreEqual ("0", s0.Get ());
             Assert.Throws<InvalidOperationException> (() => s1.Get ());
             Assert.AreEqual ("2", s2.Get ());
 
             s0.Remove ();
+            Assert.Throws<InvalidOperationException> (() => s0.Get ());
+            Assert.Throws<InvalidOperationException> (() => s1.Get ());
+            Assert.AreEqual ("2", s2.Get ());
+
             Wait ();
             Assert.Throws<InvalidOperationException> (() => s0.Get ());
             Assert.Throws<InvalidOperationException> (() => s1.Get ());
             Assert.AreEqual ("2", s2.Get ());
 
             s2.Remove ();
+            Assert.Throws<InvalidOperationException> (() => s0.Get ());
+            Assert.Throws<InvalidOperationException> (() => s1.Get ());
+            Assert.Throws<InvalidOperationException> (() => s2.Get ());
+
             Wait ();
             Assert.Throws<InvalidOperationException> (() => s0.Get ());
             Assert.Throws<InvalidOperationException> (() => s1.Get ());
@@ -146,8 +166,11 @@ namespace KRPC.Client.Test
         public void RemoveStreamTwice ()
         {
             var s = connection.AddStream (() => connection.TestService ().Int32ToString (0));
+            Assert.AreEqual ("0", s.Get ());
+
             Wait ();
             Assert.AreEqual ("0", s.Get ());
+
             s.Remove ();
             Assert.Throws<InvalidOperationException> (() => s.Get ());
             s.Remove ();
@@ -159,12 +182,18 @@ namespace KRPC.Client.Test
         {
             var s0 = connection.AddStream (() => connection.TestService ().Int32ToString (42));
             var streamId = s0.Id;
+            Assert.AreEqual ("42", s0.Get ());
+
             Wait ();
             Assert.AreEqual ("42", s0.Get ());
 
             var s1 = connection.AddStream (() => connection.TestService ().Int32ToString (42));
             Assert.AreEqual (streamId, s1.Id);
+            Assert.AreEqual ("42", s0.Get ());
+            Assert.AreEqual ("42", s1.Get ());
+
             Wait ();
+            Assert.AreEqual ("42", s0.Get ());
             Assert.AreEqual ("42", s1.Get ());
         }
     }
--- client/python/krpc/test/test_stream.py
+++ client/python/krpc/test/test_stream.py
@@ -16,41 +16,41 @@ class TestStream(ServerTestCase, unittest.TestCase):
         super(TestStream, self).setUp()
 
     def wait(self):
-        time.sleep(0.025)
+        time.sleep(0.05)
 
     def test_method(self):
         with self.conn.stream(self.conn.test_service.float_to_string, 3.14159) as x:
             for i in range(5):
-                self.wait()
                 self.assertEqual('3.14159', x())
+                self.wait()
 
     def test_property(self):
         self.conn.test_service.string_property = 'foo'
         with self.conn.stream(getattr, self.conn.test_service, 'string_property') as x:
             for i in range(5):
-                self.wait()
                 self.assertEqual('foo', x())
+                self.wait()
 
     def test_class_method(self):
         obj = self.conn.test_service.create_test_object('bob')
         with self.conn.stream(obj.float_to_string, 3.14159) as x:
             for i in range(5):
-                self.wait()
                 self.assertEqual('bob3.14159', x())
+                self.wait()
 
     def test_class_static_method(self):
         with self.conn.stream(self.conn.test_service.TestClass.static_method, 'foo') as x:
             for i in range(5):
-                self.wait()
                 self.assertEqual('jebfoo', x())
+                self.wait()
 
     def test_class_property(self):
         obj = self.conn.test_service.create_test_object('jeb')
         obj.int_property = 42
         with self.conn.stream(getattr, obj, 'int_property') as x:
             for i in range(5):
-                self.wait()
                 self.assertEqual(42, x())
+                self.wait()
 
     def test_property_setters_are_invalid(self):
         self.assertRaises(ValueError, self.conn.add_stream, setattr, self.conn.test_service, 'string_property')
@@ -58,49 +58,69 @@ class TestStream(ServerTestCase, unittest.TestCase):
         self.assertRaises(ValueError, self.conn.add_stream, setattr, obj.int_property, 42)
 
     def test_counter(self):
-        count = 0
+        count = -1
         with self.conn.stream(self.conn.test_service.counter) as x:
             for i in range(5):
-                self.wait()
                 self.assertLess(count, x())
                 count = x()
+                self.wait()
 
     def test_nested(self):
         with self.conn.stream(self.conn.test_service.float_to_string, 0.123) as x0:
             with self.conn.stream(self.conn.test_service.float_to_string, 1.234) as x1:
                 for i in range(5):
-                    self.wait()
                     self.assertEqual('0.123', x0())
                     self.assertEqual('1.234', x1())
+                    self.wait()
 
     def test_interleaved(self):
         s0 = self.conn.add_stream(self.conn.test_service.int32_to_string, 0)
+        self.assertEqual('0', s0())
+
         self.wait()
         self.assertEqual('0', s0())
 
         s1 = self.conn.add_stream(self.conn.test_service.int32_to_string, 1)
+        self.assertEqual('0', s0())
+        self.assertEqual('1', s1())
+
         self.wait()
         self.assertEqual('0', s0())
         self.assertEqual('1', s1())
 
         s1.remove()
+        self.assertEqual('0', s0())
+        self.assertRaises(RuntimeError, s1)
+
         self.wait()
         self.assertEqual('0', s0())
         self.assertRaises(RuntimeError, s1)
 
         s2 = self.conn.add_stream(self.conn.test_service.int32_to_string, 2)
+        self.assertEqual('0', s0())
+        self.assertRaises(RuntimeError, s1)
+        self.assertEqual('2', s2())
+
         self.wait()
         self.assertEqual('0', s0())
         self.assertRaises(RuntimeError, s1)
         self.assertEqual('2', s2())
 
         s0.remove()
+        self.assertRaises(RuntimeError, s0)
+        self.assertRaises(RuntimeError, s1)
+        self.assertEqual('2', s2())
+
         self.wait()
         self.assertRaises(RuntimeError, s0)
         self.assertRaises(RuntimeError, s1)
         self.assertEqual('2', s2())
 
         s2.remove()
+        self.assertRaises(RuntimeError, s0)
+        self.assertRaises(RuntimeError, s1)
+        self.assertRaises(RuntimeError, s2)
+
         self.wait()
         self.assertRaises(RuntimeError, s0)
         self.assertRaises(RuntimeError, s1)
@@ -108,8 +128,11 @@ class TestStream(ServerTestCase, unittest.TestCase):
 
     def test_remove_stream_twice(self):
         s = self.conn.add_stream(self.conn.test_service.int32_to_string, 0)
+        self.assertEqual('0', s())
+
         self.wait()
         self.assertEqual('0', s())
+
         s.remove()
         self.assertRaises(RuntimeError, s)
         s.remove()
@@ -118,12 +141,16 @@ class TestStream(ServerTestCase, unittest.TestCase):
     def test_add_stream_twice(self):
         s0 = self.conn.add_stream(self.conn.test_service.int32_to_string, 42)
         stream_id = s0._stream_id
+        self.assertEqual('42', s0())
         self.wait()
         self.assertEqual('42', s0())
 
         s1 = self.conn.add_stream(self.conn.test_service.int32_to_string, 42)
         self.assertEqual(stream_id, s1._stream_id)
+        self.assertEqual('42', s0())
+        self.assertEqual('42', s1())
         self.wait()
+        self.assertEqual('42', s0())
         self.assertEqual('42', s1())
 
 if __name__ == '__main__':
