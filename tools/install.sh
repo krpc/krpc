9baff9f310ff27a43524d89721d4dcd39690c1d3
djungelorm <djungelorm@users.noreply.github.com>
2016-02-24 21:10:27 -0800

    Copy file not symlink in install script

--- tools/install.sh
+++ tools/install.sh
@@ -23,7 +23,7 @@ cp -R \
     bazel-bin/service/**/*.dll \
     bazel-bin/service/**/*.xml \
     bazel-bin/service/**/*.json \
-    bazel-krpc/external/csharp_protobuf_net35/file/Google.Protobuf.dll \
+    bazel-krpc/external/csharp_protobuf_net35/Google.Protobuf.dll \
     bazel-bin/tools/TestingTools/TestingTools.dll \
     $GAMEDATA/
 
