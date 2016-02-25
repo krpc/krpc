8cb0bd3df722e230a14cbcc7fa744234e2f61619
djungelorm <djungelorm@users.noreply.github.com>
2016-02-24 20:54:44 -0800

    Clean up krpc-docgen

--- tools/krpctools/krpctools/docgen/__init__.py
+++ tools/krpctools/krpctools/docgen/__init__.py
@@ -15,6 +15,62 @@ from .java import JavaDomain
 from .nodes import Service
 from .docparser import DocumentationParser
 from .extensions import AppendExtension
+from ..version import __version__
+
+def main():
+    prog = 'krpc-docgen'
+    parser = argparse.ArgumentParser(prog=prog, description='Generate API for a kRPC service')
+    parser.add_argument('-v', '--version', action='version', version='%s version %s' % (prog, __version__))
+    parser.add_argument('language', choices=('cpp', 'csharp', 'lua', 'python', 'java'), help='Language to generate')
+    parser.add_argument('source', action='store', help='Path to source file')
+    parser.add_argument('order_file', action='store', default='order.txt', help='Path to order definition file')
+    parser.add_argument('destination', action='store', help='Path to destination file')
+    parser.add_argument('definitions', nargs='*', default=[], help='Paths to service definition files')
+    parser.add_argument('--no-warnings', action='store_true', default=False, help='Ignore warnings')
+    parser.add_argument('--force', action='store_true', default=False, help='Always overwrite existing files')
+    parser.add_argument('--macros', action='store', default=None, help='Path to macros template file')
+    args = parser.parse_args()
+
+    macros = args.macros
+    if macros == None:
+        macros = resource_filename(__name__, '%s.tmpl' % args.language).decode('utf-8')
+
+    if args.language == 'cpp':
+        domain = CppDomain(macros)
+    elif args.language == 'csharp':
+        domain = CsharpDomain(macros)
+    elif args.language == 'lua':
+        domain = LuaDomain(macros)
+    elif args.language == 'python':
+        domain = PythonDomain(macros)
+    else: # java
+        domain = JavaDomain(macros)
+
+    if not os.path.exists(args.order_file):
+        raise RuntimeError('Ordering file \'%s\' does not exist' % args.order_file)
+    with open(args.order_file, 'r') as f:
+        ordering = [x.strip() for x in f.readlines()]
+
+    services_info = {}
+    for definition in args.definitions:
+        for path in glob.glob(definition):
+            with open(path, 'r') as f:
+                services_info.update(json.load(f))
+
+    if services_info == {}:
+        print 'No services found in services definition files'
+        exit(1)
+
+    services = [Service(name, **info) for name,info in services_info.items()]
+    for service in services:
+        service.sort(ordering)
+    services = dict([(service.name,service) for service in services])
+
+    content = process_file(args, domain, services, args.source)
+    if not os.path.exists(os.path.dirname(args.destination)):
+        os.makedirs(os.path.dirname(args.destination))
+    with codecs.open(args.destination, 'w', encoding='utf8') as f:
+        f.write(content)
 
 def process_file(args, domain, services, path):
 
@@ -74,59 +130,6 @@ def process_file(args, domain, services, path):
         raise RuntimeError ('Don\'t know how to order:\n'+'\n'.join(krpctools.docgen.nodes.sort_members_failed))
 
     return content.rstrip()+'\n'
-
-def main():
-    parser = argparse.ArgumentParser(description='Generate API documentation from service definitions')
-    parser.add_argument('language', choices = ['cpp', 'csharp', 'lua', 'python', 'java'], help='Language to compile')
-    parser.add_argument('source', action='store', help='Path to source file')
-    parser.add_argument('order_file', action='store', default='order.txt', help='Path to order definition file')
-    parser.add_argument('destination', action='store', help='Path to destination file')
-    parser.add_argument('definitions', nargs='*', default=[], help='Paths to service definition files')
-    parser.add_argument('--no-warnings', action='store_true', default=False, help='Ignore warnings')
-    parser.add_argument('--force', action='store_true', default=False, help='Always overwrite existing files')
-    parser.add_argument('--macros', action='store', default=None, help='Path to macros template file')
-    args = parser.parse_args()
-
-    macros = args.macros
-    if macros == None:
-        macros = resource_filename(__name__, '%s.tmpl' % args.language).decode('utf-8')
-
-    if args.language == 'cpp':
-        domain = CppDomain(macros)
-    elif args.language == 'csharp':
-        domain = CsharpDomain(macros)
-    elif args.language == 'lua':
-        domain = LuaDomain(macros)
-    elif args.language == 'python':
-        domain = PythonDomain(macros)
-    else: # java
-        domain = JavaDomain(macros)
-
-    if not os.path.exists(args.order_file):
-        raise RuntimeError('Ordering file \'%s\' does not exist' % args.order_file)
-    with open(args.order_file, 'r') as f:
-        ordering = [x.strip() for x in f.readlines()]
-
-    services_info = {}
-    for definition in args.definitions:
-        for path in glob.glob(definition):
-            with open(path, 'r') as f:
-                services_info.update(json.load(f))
-
-    if services_info == {}:
-        print 'No services found in services definition files'
-        exit(1)
-
-    services = [Service(name, **info) for name,info in services_info.items()]
-    for service in services:
-        service.sort(ordering)
-    services = dict([(service.name,service) for service in services])
-
-    content = process_file(args, domain, services, args.source)
-    if not os.path.exists(os.path.dirname(args.destination)):
-        os.makedirs(os.path.dirname(args.destination))
-    with codecs.open(args.destination, 'w', encoding='utf8') as f:
-        f.write(content)
 
 if __name__ == '__main__':
     main()
