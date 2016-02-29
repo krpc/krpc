84c277cd7a762ac4898e8760ac4bd41480491ba1
djungelorm <djungelorm@users.noreply.github.com>
2016-02-28 18:23:34 -0800

    Fix test

--- service/SpaceCenter/test/test_spacecenter.py
+++ service/SpaceCenter/test/test_spacecenter.py
@@ -59,7 +59,7 @@ class TestSpaceCenter(testingtools.TestCase):
         active = self.sc.active_vessel
         active.name = 'Active'
         vessels = self.sc.vessels
-        self.assertEqual(['Active', 'Basic'], sorted(v.name for v in vessels))
+        self.assertEqual(['Active', 'OtherVessel'], sorted(v.name for v in vessels))
         self.assertEqual(self.sc.vessels, vessels)
 
     def test_bodies(self):
