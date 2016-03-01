ad5bd29a5a5efb2657fe5ee0f62bd06f9d579474
djungelorm <djungelorm@users.noreply.github.com>
2016-02-29 20:52:24 -0800

    Add script to generate genfiles archive (so that people don't need to run Bazel to build the DLLs)

---
+++ tools/dist/genfiles.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+bazel build //:csproj
+
+VERSION=`tools/get-version.sh`
+FILE=bazel-bin/krpc-genfiles-$VERSION.zip
+
+rm -f $FILE
+zip $FILE \
+    bazel-bin/server/AssemblyInfo.cs \
+    bazel-genfiles/protobuf/KRPC.cs \
+    bazel-bin/client/csharp/AssemblyInfo.cs \
+    bazel-genfiles/protobuf/KRPC.cs \
+    bazel-genfiles/client/csharp/Services/* \
+    bazel-bin/service/*/AssemblyInfo.cs \
+    bazel-bin/tools/*/AssemblyInfo.cs \
+    bazel-krpc/external/csharp_protobuf/Google.Protobuf.nuspec \
+    bazel-krpc/external/csharp_protobuf/lib/dotnet/Google.Protobuf.* \
+    bazel-krpc/external/csharp_protobuf_net35/Google.Protobuf.* \
+    bazel-krpc/external/csharp_protobuf_net35/file/Google.Protobuf.* \
+    bazel-krpc/external/csharp_nunit/license.txt \
+    bazel-krpc/external/csharp_nunit/bin/framework/nunit.framework.* \
+    bazel-krpc/external/csharp_moq/Moq.nuspec \
+    bazel-krpc/external/csharp_moq/lib/net40/Moq.* \
+    bazel-krpc/external/csharp_json/license.txt \
+    bazel-krpc/external/csharp_json/Bin/Net45/Newtonsoft.Json.* \
+    bazel-krpc/external/csharp_options/NDesk.Options.nuspec \
+    bazel-krpc/external/csharp_options/lib/NDesk.Options.*
