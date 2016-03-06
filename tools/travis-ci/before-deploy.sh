264e5d8b1c8d68c77aa8bd06684872c15110d718
djungelorm <djungelorm@users.noreply.github.com>
2016-03-06 12:10:29 -0800

    Deploy krpc-*.zip and its contents to S3 when a build is run, in a location specific to the build

---
+++ tools/travis-ci/before-deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ev
+mkdir deploy
+
+if [[ $TRAVIS_PULL_REQUEST != "false" ]]; then
+    NAME="pr/"$TRAVIS_PULL_REQUEST
+else
+    NAME=$TRAVIS_BRANCH
+fi
+DEPLOYPATH=deploy/$NAME/$TRAVIS_JOB_NUMBER
+VERSION=`tools/get-version.sh`
+mkdir -p $DEPLOYPATH
+cp bazel-bin/krpc-$VERSION.zip $DEPLOYPATH/
+(cd $DEPLOYPATH; unzip *.zip)
