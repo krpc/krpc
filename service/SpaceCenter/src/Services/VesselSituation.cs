f8bc4dcbc94b6c7f7cbc8788f6b1d94897530281
djungelorm <djungelorm@users.noreply.github.com>
2016-04-02 10:52:49 -0700

    Refactor enums into separate source files

---
+++ service/SpaceCenter/src/Services/VesselSituation.cs
@@ -0,0 +1,44 @@
+﻿using KRPC.Service.Attributes;
+
+namespace KRPC.SpaceCenter.Services
+{
+    /// <summary>
+    /// See <see cref="Vessel.Situation"/>.
+    /// </summary>
+    [KRPCEnum (Service = "SpaceCenter")]
+    public enum VesselSituation
+    {
+        /// <summary>
+        /// Vessel is awaiting launch.
+        /// </summary>
+        PreLaunch,
+        /// <summary>
+        /// Vessel is orbiting a body.
+        /// </summary>
+        Orbiting,
+        /// <summary>
+        /// Vessel is on a sub-orbital trajectory.
+        /// </summary>
+        SubOrbital,
+        /// <summary>
+        /// Escaping.
+        /// </summary>
+        Escaping,
+        /// <summary>
+        /// Vessel is flying through an atmosphere.
+        /// </summary>
+        Flying,
+        /// <summary>
+        /// Vessel is landed on the surface of a body.
+        /// </summary>
+        Landed,
+        /// <summary>
+        /// Vessel has splashed down in an ocean.
+        /// </summary>
+        Splashed,
+        /// <summary>
+        /// Vessel is docked to another.
+        /// </summary>
+        Docked
+    }
+}
---
+++ service/SpaceCenter/src/Services/WarpMode.cs
@@ -0,0 +1,24 @@
+﻿using KRPC.Service.Attributes;
+
+namespace KRPC.SpaceCenter
+{
+    /// <summary>
+    /// Returned by <see cref="SpaceCenter.WarpMode"/>
+    /// </summary>
+    [KRPCEnum (Service = "SpaceCenter")]
+    public enum WarpMode
+    {
+        /// <summary>
+        /// Time warp is active, and in regular "on-rails" mode.
+        /// </summary>
+        Rails,
+        /// <summary>
+        /// Time warp is active, and in physical time warp mode.
+        /// </summary>
+        Physics,
+        /// <summary>
+        /// Time warp is not active.
+        /// </summary>
+        None
+    }
+}
