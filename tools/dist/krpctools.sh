9b3585c58c71269ca929cbd799889b1ed473b5c7
djungelorm <djungelorm@users.noreply.github.com>
2016-04-20 22:13:29 -0700

    Update dist script [ci skip]

--- tools/dist/krpctools.sh
+++ tools/dist/krpctools.sh
@@ -8,6 +8,6 @@ set -e
 TWINE=twine
 VERSION=`tools/krpc-version.sh`
 
-bazel build //tools/clientgen:krpc.clientgen
+bazel build //tools/krpc-tools:krpctools
 
-$TWINE upload bazel-bin/tools/clientgen/krpc.clientgen-$VERSION.zip
+$TWINE upload bazel-bin/tools/krpctools/krpctools-$VERSION.zip
