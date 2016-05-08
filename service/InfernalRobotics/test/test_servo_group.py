22747e0cbca200a8f69a48e9f7c6b77d391e43be
djungelorm <djungelorm@users.noreply.github.com>
2016-05-08 16:55:55 -0700

    Add InfernalRobotics tests

---
+++ service/InfernalRobotics/test/test_servo_group.py
@@ -0,0 +1,35 @@
+import unittest
+import testingtools
+import krpc
+import time
+
+class TestServoGroup(testingtools.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        testingtools.new_save()
+        testingtools.launch_vessel_from_vab('InfernalRobotics')
+        testingtools.remove_other_vessels()
+        cls.conn = testingtools.connect(name='TestServoGroup')
+        cls.ir = cls.conn.infernal_robotics
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.conn.close()
+
+    def test_servo_group(self):
+        group1 = self.ir.servo_group_with_name('Group1')
+        group2 = self.ir.servo_group_with_name('Group2')
+        self.assertEqual('Group1', group1.name)
+        self.assertEqual(['Hinge', 'Rail', 'Rotatron'], sorted(x.name for x in group1.servos))
+        self.assertEqual('Group2', group2.name)
+        self.assertEqual(['DockingFree', 'DockingRotatron'], sorted(x.name for x in group2.servos))
+
+    def test_servo_with_name(self):
+        group = self.ir.servo_group_with_name('Group1')
+        servo = group.servo_with_name('Rotatron')
+        self.assertEqual(servo.name, 'Rotatron')
+        self.assertIsNone (group.servo_with_name('Foo'))
+
+if __name__ == '__main__':
+    unittest.main()
