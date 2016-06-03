b278e11388b4bc98bd304c64d577cbb22c570a40
djungelorm <djungelorm@users.noreply.github.com>
2016-06-02 20:51:39 -0700

    Use mono system libs in place of KSP system libs on travis

--- tools/travis-ci/install-ksp-libs.sh
+++ tools/travis-ci/install-ksp-libs.sh
@@ -5,3 +5,4 @@ mkdir -p lib/ksp
 wget -O lib/ksp/ksp-$VERSION.tar.gz https://s3.amazonaws.com/krpc/lib/ksp-$VERSION.tar.gz
 (cd lib/ksp; tar -xf ksp-$VERSION.tar.gz)
 rm lib/ksp/ksp-$VERSION.tar.gz
+cp lib/mono-4.5/mscorlib.dll lib/mono-4.5/System.Core.dll lib/mono-4.5/System.dll lib/mono-4.5/System.Xml.dll lib/mono-4.5/System.Xml.Linq.dll lib/ksp/KSP_Data/Managed
