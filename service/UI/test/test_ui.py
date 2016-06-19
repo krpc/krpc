3567f135bde70587dfc821c588c85c03b95b03f5
djungelorm <djungelorm@users.noreply.github.com>
2016-06-19 10:39:06 -0700

    Update UI service tests

--- service/UI/test/test_ui.py
+++ service/UI/test/test_ui.py
@@ -4,20 +4,15 @@ import krpctest
 class TestUI(krpctest.TestCase):
 
     @classmethod
-    def setUp(cls):
-        krpctest.new_save()
-        cls.conn = krpctest.connect(cls)
-        cls.ui = cls.conn.ui
-        cls.message_position = cls.conn.ui.MessagePosition
-
-    @classmethod
-    def tearDown(cls):
-        cls.conn.close()
+    def setUpClass(cls):
+        cls.new_save()
+        cls.ui = cls.connect().ui
+        cls.message_position = cls.ui.MessagePosition
 
     def test_message(self):
-        self.ui.message("One")
-        self.ui.message("Two", 5)
-        self.ui.message("Three", 1, self.message_position.top_right)
+        self.ui.message('One')
+        self.ui.message('Two', 5)
+        self.ui.message('Three', 1, self.message_position.top_right)
 
 if __name__ == '__main__':
     unittest.main()
