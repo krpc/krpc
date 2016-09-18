22dd617155c3c05b547c8b8acf35e80436bebe4b
djungelorm <djungelorm@users.noreply.github.com>
2016-09-17 21:21:17 -0700

    Add extension method to provide old style ITorqueProvider.GetPotentialTorque() method

---
+++ service/SpaceCenter/src/ExtensionMethods/ITorqueProviderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace KRPC.SpaceCenter
+{
+    /// <summary>
+    /// Extension methods for torque providers
+    /// </summary>
+    public static class ITorqueProviderExtensions
+    {
+        /// <summary>
+        /// Get the largest potential torque provided by the part.
+        /// </summary>
+        public static Vector3 GetPotentialTorque (this ITorqueProvider torqueProvider)
+        {
+            Vector3 pos;
+            Vector3 neg;
+            torqueProvider.GetPotentialTorque (out pos, out neg);
+            return pos.sqrMagnitude >= neg.sqrMagnitude ? pos : neg;
+        }
+    }
+}
