010d87b406206552abccf93d0697083a7fa57425
djungelorm <djungelorm@users.noreply.github.com>
2016-10-02 19:03:43 -0700

    Fix linter warnings

--- service/SpaceCenter/test/test_parts_landing_gear.py
+++ service/SpaceCenter/test/test_parts_landing_gear.py
@@ -10,32 +10,32 @@ class TestPartsLandingGear(krpctest.TestCase):
         self.parts = self.connect().space_center.active_vessel.parts
         self.gear = self.parts.with_title('LY-99 Extra Large Landing Gear')[0].landing_gear
         self.fixed_gear = self.parts.with_title('LY-01 Fixed Landing Gear')[0].landing_gear
-        self.State = self.connect().space_center.LandingGearState
+        self.state = self.connect().space_center.LandingGearState
 
     def test_deploy_and_retract(self):
         self.assertTrue(self.gear.deployable)
-        self.assertEqual(self.State.deployed, self.gear.state)
+        self.assertEqual(self.state.deployed, self.gear.state)
         self.assertTrue(self.gear.deployed)
         self.gear.deployed = False
         self.wait()
-        self.assertEqual(self.State.retracting, self.gear.state)
+        self.assertEqual(self.state.retracting, self.gear.state)
         self.assertFalse(self.gear.deployed)
-        while self.gear.state == self.State.retracting:
+        while self.gear.state == self.state.retracting:
             self.wait()
-        self.assertEqual(self.State.retracted, self.gear.state)
+        self.assertEqual(self.state.retracted, self.gear.state)
         self.assertFalse(self.gear.deployed)
         self.gear.deployed = True
         self.wait()
-        self.assertEqual(self.State.deploying, self.gear.state)
+        self.assertEqual(self.state.deploying, self.gear.state)
         self.assertFalse(self.gear.deployed)
-        while self.gear.state == self.State.deploying:
+        while self.gear.state == self.state.deploying:
             self.wait()
-        self.assertEqual(self.State.deployed, self.gear.state)
+        self.assertEqual(self.state.deployed, self.gear.state)
         self.assertTrue(self.gear.deployed)
 
     def test_fixed_gear(self):
         self.assertFalse(self.fixed_gear.deployable)
-        self.assertEqual(self.State.deployed, self.fixed_gear.state)
+        self.assertEqual(self.state.deployed, self.fixed_gear.state)
         self.assertTrue(self.fixed_gear.deployed)
 
     def test_grounded(self):
