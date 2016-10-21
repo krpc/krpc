c9599005291d79b6101bce00cfe39296e7c01110
djungelorm <djungelorm@users.noreply.github.com>
2016-10-21 00:20:19 -0700

    Improve serve-docs script

--- tools/serve-docs.sh
+++ tools/serve-docs.sh
@@ -5,8 +5,25 @@
 set -e
 
 port=${1:-8080}
-dir=bazel-bin/doc/html
-bazel build //doc:html
-rm -rf $dir
-unzip -q bazel-bin/doc/html.zip -d $dir
-(cd $dir; python -m SimpleHTTPServer $port)
+src=bazel-bin/doc/serve/src
+env=bazel-bin/doc/serve/env
+out=bazel-bin/doc/serve/out
+
+bazel build //doc:srcs
+
+mkdir -p `dirname $env`
+rm -rf $src
+cp -R bazel-genfiles/doc/srcs $src
+
+if [ ! -d "$env" ]; then
+  virtualenv $env --system-site-packages
+  source $env/bin/activate
+  pip install \
+    sphinx==1.3.5 sphinx-autobuild sphinx_rtd_theme sphinxcontrib_spelling sphinx-csharp \
+    https://github.com/djungelorm/sphinx-lua/releases/download/0.1.2/sphinx-lua-0.1.2.tar.gz \
+    javasphinx
+else
+  source $env/bin/activate
+fi
+
+sphinx-autobuild $src $out
