a3a8a89ce98c3ed5c42aeb242cfa6830754d220b
djungelorm <djungelorm@users.noreply.github.com>
2016-12-11 21:19:50 +0000

    Download and install bazel using .deb, and checksum the downloaded file

--- tools/travis-ci/install-bazel.sh
+++ tools/travis-ci/install-bazel.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -ev
+
 VERSION=0.4.1
-wget https://github.com/bazelbuild/bazel/releases/download/$VERSION/bazel-$VERSION-jdk7-installer-linux-x86_64.sh
-chmod +x bazel-$VERSION-jdk7-installer-linux-x86_64.sh
-./bazel-$VERSION-jdk7-installer-linux-x86_64.sh --user
-rm ./bazel-$VERSION-jdk7-installer-linux-x86_64.sh
+HASH=a2ea9a5789713d79f439c54a93177e58136cd622fdcad8d2906d3448303bd937
+
+DEB=bazel_$VERSION-linux-x86_64.deb
+wget https://github.com/bazelbuild/bazel/releases/download/$VERSION/$DEB
+echo "$HASH  $DEB" > ./$DEB.sha256
+sha256sum -c ./$DEB.sha256
+rm $DEB.sha256
+sudo dpkg -i ./bazel_$VERSION-linux-x86_64.deb
+rm $DEB
 cat tools/travis-ci/bazelrc >> .bazelrc
