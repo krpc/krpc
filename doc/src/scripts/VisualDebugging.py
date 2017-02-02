662c0ba689fd8fa7d65638bda616ed07c7b271db
djungelorm <djungelorm@users.noreply.github.com>
2017-02-02 17:55:33 +0000

    Update visual debugging example

--- doc/src/scripts/VisualDebugging.py
+++ doc/src/scripts/VisualDebugging.py
@@ -2,9 +2,7 @@ import krpc
 conn = krpc.connect(name='Visual Debugging')
 vessel = conn.space_center.active_vessel
 
-ref_frame = vessel.orbit.body.reference_frame
-velocity = vessel.flight(ref_frame).velocity
-conn.drawing.add_direction(velocity, ref_frame)
-
+ref_frame = vessel.surface_velocity_reference_frame
+conn.drawing.add_direction((0, 1, 0), ref_frame)
 while True:
     pass
