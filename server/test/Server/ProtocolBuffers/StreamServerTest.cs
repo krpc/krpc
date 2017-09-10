d8c727fd63959c4d9ed071588eee5d5ef5b6b944
djungelorm <djungelorm@users.noreply.github.com>
2017-09-10 14:27:35 +0100

    Fix exception message in tests (changed in protobuf v3.4.0)

--- server/test/Server/ProtocolBuffers/StreamServerTest.cs
+++ server/test/Server/ProtocolBuffers/StreamServerTest.cs
@@ -107,7 +107,7 @@ namespace KRPC.Test.Server.ProtocolBuffers
 
             TestingTools.CheckConnectionResponse (responseStream.ToArray (), 209, Status.MalformedMessage,
                 "While parsing a protocol message, the input ended unexpectedly in the middle of a field.  " +
-                "This could mean either than the input has been truncated or that an embedded message misreported its own length.", 0);
+                "This could mean either that the input has been truncated or that an embedded message misreported its own length.", 0);
         }
 
         [Test]
