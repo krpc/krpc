7ba8c50c244c61069298e034e25c7bac0e0e11b4
djungelorm <djungelorm@users.noreply.github.com>
2017-09-25 21:22:46 +0100

    Fix reserved keyword clashes in C++ docgen

--- tools/krpctools/krpctools/docgen/cpp.py
+++ tools/krpctools/krpctools/docgen/cpp.py
@@ -15,6 +15,23 @@ class CppDomain(Domain):
     sphinxname = 'cpp'
     codeext = 'cpp'
 
+    _keywords = set([
+        'alignas', 'alignof', 'and', 'and_eq', 'asm', 'auto', 'bitand',
+        'bitor', 'bool', 'break', 'case', 'catch', 'char', 'char16_t',
+        'char32_t', 'class', 'compl', 'concept', 'const', 'constexpr',
+        'const_cast', 'continue', 'decltype', 'default', 'delete', 'do',
+        'double', 'dynamic_cast', 'else', 'enum', 'explicit', 'export',
+        'extern', 'false', 'float', 'for', 'friend', 'goto', 'if', 'inline',
+        'int', 'long', 'mutable', 'namespace', 'new', 'noexcept', 'not',
+        'not_eq', 'nullptr', 'operator', 'or', 'or_eq', 'private', 'protected',
+        'public', 'register', 'reinterpret_cast', 'requires', 'return',
+        'short', 'signed', 'sizeof', 'static', 'static_assert', 'static_cast',
+        'struct', 'switch', 'template', 'this', 'thread_local', 'throw',
+        'true', 'try', 'typedef', 'typeid', 'typename', 'union', 'unsigned',
+        'using', 'virtual', 'void', 'volatile', 'wchar_t', 'while', 'xor',
+        'xor_eq'
+    ])
+
     type_map = {
         Type.DOUBLE: 'double',
         Type.FLOAT: 'float',
@@ -37,6 +54,12 @@ class CppDomain(Domain):
     def currentmodule(self, name):
         super(CppDomain, self).currentmodule(name)
         return '.. namespace:: krpc::services::%s' % name
+
+    def method_name(self, name):
+        if snake_case(name) in self._keywords:
+            return '%s_' % name
+        else:
+            return name
 
     def type(self, typ):
         if typ is None:
