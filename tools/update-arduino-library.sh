f3b9fd86171be5251cd55001c6b192586c3c3f66
djungelorm <djungelorm@users.noreply.github.com>
2017-10-25 21:27:49 +0100

    Update arduino library file structure to match 1.5 spec [ci skip]

--- tools/update-arduino-library.sh
+++ tools/update-arduino-library.sh
@@ -13,9 +13,10 @@ rm -rf $arduino
 git clone git@github.com:krpc/krpc-arduino $arduino
 
 # Update arduino library
-rm -rf $arduino/*.h $arduino/*.cpp $arduino/krpc
-cp bazel-bin/client/cnano/krpc-cnano-$VERSION.zip $arduino/
-cd $arduino
+rm -rf $arduino/src
+mkdir $arduino/src
+cp bazel-bin/client/cnano/krpc-cnano-$VERSION.zip $arduino/src/
+cd $arduino/src
 unzip krpc-cnano-$VERSION.zip
 mv krpc-cnano-$VERSION/include/* ./
 mv krpc-cnano-$VERSION/src/*.c ./
@@ -24,6 +25,7 @@ for file in *.c; do
   mv "$file" "$(basename "$file" .c).cpp"
 done
 rm -rf krpc-cnano-$VERSION krpc-cnano-$VERSION.zip
+cd $arduino
 git add .
 git diff-index --quiet HEAD || git commit -m "Updated from https://github.com/krpc/krpc commit $COMMIT"
 #git push origin master
