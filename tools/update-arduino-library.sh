c0decfb09ed0004033a2dd34467e3e3c0f59cc11
djungelorm <djungelorm@users.noreply.github.com>
2017-10-26 18:47:11 +0100

    Improve arduino library update script

--- tools/update-arduino-library.sh
+++ tools/update-arduino-library.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
-set -ev
+set -e
 
 VERSION=`tools/krpc-version.sh`
 COMMIT=`git rev-parse HEAD`
 
 # Build cnano library
+echo 'Building library...'
 bazel build //client/cnano
 
 # Clone arduino library repository
 arduino=`pwd`/bazel-genfiles/client/cnano/arduino
-rm -rf $arduino
-git clone git@github.com:krpc/krpc-arduino $arduino
+if [ ! -d $arduino ]; then
+  echo 'Cloning krpc-arduino repository...'
+  git clone git@github.com:krpc/krpc-arduino $arduino
+fi
 
 # Update src files
+echo 'Updating src files...'
 rm -rf $arduino/src
 mkdir $arduino/src
 cp bazel-bin/client/cnano/krpc-cnano-$VERSION.zip $arduino/src/
 cd $arduino/src
-unzip krpc-cnano-$VERSION.zip
+unzip -q krpc-cnano-$VERSION.zip
 mv krpc-cnano-$VERSION/include/* ./
 mv krpc-cnano-$VERSION/src/*.c ./
 rm Makefile.*
@@ -28,8 +32,16 @@ rm -rf krpc-cnano-$VERSION krpc-cnano-$VERSION.zip
 cd $arduino
 
 # Update library.properties
+echo "Updating library.properties to v${VERSION}"
 sed -i -e "s/version=.*/version=${VERSION}/g" library.properties
 
-git add .
-git diff-index --quiet HEAD || git commit -m "Updated from https://github.com/krpc/krpc commit $COMMIT"
-git push origin master
+if [ "$1" == "push" ]; then
+  echo 'Pushing changes...'
+  git add .
+  git diff-index --quiet HEAD || git commit -m "Updated from https://github.com/krpc/krpc commit $COMMIT"
+  git push origin master
+else
+  echo "Skipping pushing changes (enable by passing 'push' as argument)"
+fi
+
+echo 'Done'
