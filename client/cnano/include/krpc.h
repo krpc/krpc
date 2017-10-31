3a5e531f8c883aac4731bb70003d2ad9096768b4
djungelorm <djungelorm@users.noreply.github.com>
2017-10-31 18:04:05 +0000

    Clean up

--- client/cnano/include/krpc.h
+++ client/cnano/include/krpc.h
@@ -11,15 +11,7 @@ extern "C" {
 /* Connect to a kRPC server using the given communication handle */
 krpc_error_t krpc_connect(krpc_connection_t connection, const char * clientName);
 
-/* Make an RPC call.
- *
- * Parameters:
- *  fd     - the file descriptor for the connection.
- *  result - message where the result will be stored
- *  call   - message describing the call
- *
- * Returns a procedure result containing the return value, if any.
- */
+/* Make an RPC call. Returns a procedure result containing the return value, if any. */
 krpc_error_t krpc_invoke(
   krpc_connection_t connection, krpc_schema_ProcedureResult * result,
   krpc_schema_ProcedureCall * call);
--- client/cnano/src/error.c
+++ client/cnano/src/error.c
@@ -6,13 +6,13 @@ void (*krpc_error_handler)(krpc_error_t);
 
 const char * krpc_get_error(krpc_error_t error) {
   switch (error) {
+  case KRPC_ERROR_IO:                return "I/O failure";
+  case KRPC_ERROR_EOF:               return "end of file";
+  case KRPC_ERROR_CONNECTION_FAILED: return "connection failed";
+  case KRPC_ERROR_NO_RESULTS:        return "no results";
+  case KRPC_ERROR_RPC_FAILED:        return "rpc failed";
   case KRPC_ERROR_ENCODING_FAILED:   return "encoding failed";
   case KRPC_ERROR_DECODING_FAILED:   return "decoding failed";
-  case KRPC_ERROR_NO_RESULTS:        return "no results";
-  case KRPC_ERROR_IO:                return "I/O failure";
-  case KRPC_ERROR_EOF:               return "unexpected eof";
-  case KRPC_ERROR_CONNECTION_FAILED: return "connection failed";
-  case KRPC_ERROR_RPC_FAILED:        return "rpc failed";
   case KRPC_ERROR_BUFFER_TOO_SMALL:  return "buffer too small";
   default: return "";
   }
