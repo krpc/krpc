dbe5f6c14a54977327c862fa1b24912d83624822
djungelorm <djungelorm@users.noreply.github.com>
2017-11-01 18:04:50 +0000

    Improve IWYU script

--- client/cpp/iwyu.sh
+++ client/cpp/iwyu.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-IWYU="include-what-you-use -Xiwyu --no_comments -std=c++11 -Iinclude -I../../bazel-genfiles/client/cpp/include -I../../bazel-krpc/external/cpp_googletest/googletest/include -I../../bazel-krpc/external/cpp_googletest/googlemock/include -I../../bazel-genfiles/client/cpp/test -I../../bazel-krpc/external/cpp_protobuf/src/google/protobuf"
+root=$( cd $(dirname $0) ; pwd -P )
+cd $root
 
-${IWYU} src/client.cpp
-${IWYU} src/connection.cpp
-${IWYU} src/decoder.cpp
-${IWYU} src/encoder.cpp
-${IWYU} src/event.cpp
-${IWYU} src/krpc.cpp
-${IWYU} src/platform.cpp
-${IWYU} src/stream_impl.cpp
-${IWYU} src/stream_manager.cpp
+iwyu="include-what-you-use -Xiwyu --no_comments -std=c++11"
+includes="-Iinclude -I../../bazel-genfiles/client/cpp/include -I../../bazel-krpc/external/cpp_googletest/googletest/include -I../../bazel-krpc/external/cpp_googletest/googlemock/include -I../../bazel-genfiles/client/cpp/test -I../../bazel-krpc/external/cpp_protobuf/src/google/protobuf"
 
-${IWYU} test/test_client.cpp
-${IWYU} test/test_decoder.cpp
-${IWYU} test/test_encodedecode.cpp
-${IWYU} test/test_encoder.cpp
-${IWYU} test/test_event.cpp
-${IWYU} test/test_object.cpp
-${IWYU} test/test_services.cpp
-${IWYU} test/test_stream.cpp
+for path in src/*.cpp; do
+  ${iwyu} ${includes} ${path}
+done
+
+for path in test/*.cpp; do
+  ${iwyu} ${includes} ${path}
+done
