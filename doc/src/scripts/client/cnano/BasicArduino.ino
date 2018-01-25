01734112aa2a7fd29ed8f69a47ab0e7391bff9ce
djungelorm <djungelorm@users.noreply.github.com>
2018-01-25 21:07:24 +0000

    Add example of how to connect cnano client on Arduino (Fix #436)

---
+++ doc/src/scripts/client/cnano/BasicArduino.ino
@@ -0,0 +1,21 @@
+#include <krpc.h>
+#include <krpc/services/krpc.h>
+
+HardwareSerial * conn;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  conn = &Serial;
+  // Open the serial port connection
+  krpc_open(&conn, NULL);
+  // Set up communication with the server
+  krpc_connect(conn, "Arduino Example");
+
+  // Indicate succesful connection by lighting the on-board LED
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void loop() {
+}
