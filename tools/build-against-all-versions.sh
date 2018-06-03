fdb6523a28a5d498254683fca3751dc5555ca5c1
djungelorm <djungelorm@users.noreply.github.com>
2018-06-03 15:45:14 +0100

    Add script to build kRPC against all versions of KSP libs

---
+++ tools/build-against-all-versions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p bazel-bin/lib
+pushd bazel-bin/lib
+
+versions="1.2.1.1604 1.2.2.1622 1.3.0.1804 1.3.1.1891 1.4.0.2077 1.4.1.2089 1.4.1.2110 1.4.1.2152"
+
+for version in $versions; do
+    if [ ! -f ksp-$version.tar.gz ]; then
+        wget -O ksp-$version.tar.gz https://krpc.s3.amazonaws.com/lib/ksp-$version.tar.gz
+    fi
+    if [ ! -d ksp-$version ]; then
+        mkdir ksp-$version
+        pushd ksp-$version
+        tar -xf ../ksp-$version.tar.gz
+        popd
+    fi
+
+    popd
+    rm -f lib/ksp
+    ln -s -r bazel-bin/lib/ksp-$version lib/ksp
+    bazel build //:krpc
+    pushd bazel-bin/lib
+done
+
+#bazel build //:krpc
