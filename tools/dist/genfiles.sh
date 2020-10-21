773c7d7688406ded2594a527fe862f97ac594a9e
kylewill0725 <kylewill0725@outlook.com>
2020-10-21 10:42:05 -0500

    Updated genfiles to use the generated_deps folder

--- tools/dist/genfiles.sh
+++ tools/dist/genfiles.sh
@@ -8,13 +8,4 @@ VERSION=`tools/krpc-version.sh`
 FILE=bazel-bin/krpc-genfiles-$VERSION.zip
 
 rm -f $FILE
-zip $FILE \
-    bazel-bin/server/AssemblyInfo.cs \
-    bazel-genfiles/protobuf/KRPC.cs \
-    bazel-bin/client/csharp/AssemblyInfo.cs \
-    bazel-genfiles/protobuf/KRPC.cs \
-    bazel-genfiles/client/csharp/Services/* \
-    bazel-bin/service/*/AssemblyInfo.cs \
-    bazel-bin/tools/*/AssemblyInfo.cs \
-    bazel-genfiles/tools/cslibs/* \
-    bazel-genfiles/tools/cslibs/**/*
+zip -r $FILE generated_deps
