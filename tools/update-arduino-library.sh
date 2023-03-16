9db2c20584119c7b3471273d567f74dd31f00fba
djungelorm <djungelorm@users.noreply.github.com>
2023-03-16 21:14:17 +0000

    Update Arduino Cnano library to use same path as Cnano client

--- tools/update-arduino-library.sh
+++ tools/update-arduino-library.sh
@@ -9,7 +9,7 @@ echo 'Building library...'
 bazel build //client/cnano
 
 # Clone arduino library repository
-arduino=`pwd`/bazel-genfiles/client/cnano/arduino
+arduino=`pwd`/bazel-bin/client/cnano/arduino
 if [ ! -d $arduino ]; then
   echo 'Cloning krpc-arduino repository...'
   git clone git@github.com:krpc/krpc-arduino $arduino
@@ -26,23 +26,15 @@ unzip -q krpc-cnano-$VERSION.zip
 mv krpc-cnano-$VERSION/include/* ./
 mv krpc-cnano-$VERSION/src/*.c ./
 rm Makefile.*
-mv krpc_cnano.h krpc.h
-mv krpc_cnano krpc
 rm -rf krpc-cnano-$VERSION krpc-cnano-$VERSION.zip
 # Rename .c to .cpp for arduino builder
 for file in *.c; do
   mv "$file" "$(basename "$file" .c).cpp"
 done
-# Fix up includes to krpc/... instead of krpc_cnano/...
-pwd
-for file in `find . -name "*.h" -o -name "*.cpp"`; do
-    sed -i -r -e "s/#include ([<\"])krpc_cnano\/(.+)([>\"])/#include \1krpc\/\2\3/g" $file
-    sed -i -e "s/#include <krpc_cnano\.h>/#include <krpc\.h>/g" $file
-    sed -i -r -e "s/#include \"(pb.*\.h)\"/#include \"krpc\/\1\"/g" $file
-done
 # Enable PB_NO_ERRMSG by default
-echo -e "#ifndef PB_NO_ERRMSG\n#define PB_NO_ERRMSG\n#endif\n\n$(cat krpc/pb.h)" > krpc/pb.h
+echo -e "#ifndef PB_NO_ERRMSG\n#define PB_NO_ERRMSG\n#endif\n\n$(cat krpc_cnano/pb.h)" > krpc_cnano/pb.h
 cd $arduino
+echo "Arduino library source in $arduino"
 
 # Update library.properties
 echo "Updating library.properties to v${VERSION}"
