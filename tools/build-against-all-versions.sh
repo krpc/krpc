a3fb63bceb7526f86ed3aaf8ac21992a07eed5f3
djungelorm <djungelorm@users.noreply.github.com>
2024-06-10 10:13:28 +0100

    Fix build-against-all-versions script

--- tools/build-against-all-versions.sh
+++ tools/build-against-all-versions.sh
@@ -7,7 +7,9 @@ mkdir -p bazel-bin/lib
 pushd bazel-bin/lib
 
 read -d '' versions << EOM || true
-1.10.1.2939
+1.12.5
+1.12.4
+1.12.3
 EOM
 
 # Save lib/ksp symlink
@@ -17,13 +19,13 @@ pushd bazel-bin/lib
 
 for version in $versions; do
     # Download and extract ksp libs
-    if [ ! -f ksp-$version.tar.gz ]; then
-        wget -O ksp-$version.tar.gz https://krpc.s3.amazonaws.com/lib/ksp-$version.tar.gz
+    if [ ! -f ksp-$version.zip ]; then
+        wget -O ksp-$version.zip https://github.com/krpc/ksp-lib/raw/main/ksp/ksp-$version.zip
     fi
     if [ ! -d ksp-$version ]; then
         mkdir ksp-$version
         pushd ksp-$version
-        tar -xf ../ksp-$version.tar.gz
+        unzip ../ksp-$version.zip
         popd
     fi
 
