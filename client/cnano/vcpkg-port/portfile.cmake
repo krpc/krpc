34bf2b0bc3ed5b92b9be6914efe743a05512ce28
djungelorm <djungelorm@users.noreply.github.com>
2026-06-20 23:03:03 +0100

    Fix package name (port name is hyphenated)

--- client/cnano/vcpkg-port/portfile.cmake
+++ client/cnano/vcpkg-port/portfile.cmake
@@ -17,7 +17,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/krpc_cnano)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/krpc_cnano PACKAGE_NAME krpc_cnano)
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
