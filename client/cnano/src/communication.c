fc199ba96e74943b2fad049d5c8aef4c3b251ce6
djungelorm <djungelorm@users.noreply.github.com>
2026-06-20 16:20:33 +0100

    Add support for windows communication to cnano client

--- client/cnano/src/communication.c
+++ client/cnano/src/communication.c
@@ -45,6 +45,49 @@ krpc_error_t krpc_write(krpc_connection_t connection, const uint8_t * buf, size_
 
 #endif
 
+#if defined(KRPC_COMMUNICATION_WINDOWS)
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+
+krpc_error_t krpc_open(krpc_connection_t * connection, const krpc_connection_config_t * arg) {
+  const char * port = arg;
+  HANDLE handle = CreateFileA(port, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+  if (handle == INVALID_HANDLE_VALUE)
+    KRPC_RETURN_ERROR(IO, "failed to open serial port");
+  *connection = handle;
+  return KRPC_OK;
+}
+
+krpc_error_t krpc_close(krpc_connection_t connection) {
+  CloseHandle((HANDLE)connection);
+  return KRPC_OK;
+}
+
+krpc_error_t krpc_read(krpc_connection_t connection, uint8_t * buf, size_t count) {
+  size_t total = 0;
+  while (total < count) {
+    DWORD bytes_read = 0;
+    if (!ReadFile((HANDLE)connection, buf + total, (DWORD)(count - total), &bytes_read, NULL))
+      KRPC_RETURN_ERROR(IO, "read failed");
+    if (bytes_read == 0)
+      KRPC_RETURN_ERROR(EOF, "eof received");
+    total += bytes_read;
+  }
+  return KRPC_OK;
+}
+
+krpc_error_t krpc_write(krpc_connection_t connection, const uint8_t * buf, size_t count) {
+  DWORD bytes_written = 0;
+  if (!WriteFile((HANDLE)connection, buf, (DWORD)count, &bytes_written, NULL) || bytes_written != (DWORD)count)
+    KRPC_RETURN_ERROR(IO, "write failed");
+  return KRPC_OK;
+}
+
+#endif
+
 #if defined(KRPC_COMMUNICATION_ARDUINO)
 
 krpc_error_t krpc_open(krpc_connection_t * connection, const krpc_connection_config_t * arg) {
