374f7172c86e7647cc480659ca7aabcda69ff2c2
Dominique Archambault <1649005+darchambault@users.noreply.github.com>
2026-06-23 00:16:59 -0400

    fixed failing tests due to outdated part name in test_parts_experiment.py

--- service/SpaceCenter/test/test_parts_experiment.py
+++ service/SpaceCenter/test/test_parts_experiment.py
@@ -16,7 +16,7 @@ class TestPartsExperiment(krpctest.TestCase):
         self.remove_other_vessels()
         self.sc = self.connect().space_center
         self.parts = self.sc.active_vessel.parts
-        self.pod = self.parts.with_name("mk1pod")[0].experiment
+        self.pod = self.parts.with_name("mk1pod.v2")[0].experiment
         self.goo = self.parts.with_name("GooExperiment")[0].experiment
 
     def test_experiments(self):
