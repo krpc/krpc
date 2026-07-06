094d823f0b697bf32b47a74c845fe7f44721a404
djungelorm <djungelorm@users.noreply.github.com>
2026-07-07 00:05:46 +0100

    Add a space between value and units in the info window data rows

--- server/src/UI/InfoWindow.cs
+++ server/src/UI/InfoWindow.cs
@@ -152,10 +152,10 @@ namespace KRPC.UI
         {
             string[] suffix = { "B", "KB", "MB", "GB", "TB", "PB", "EB" };
             if (bytes == 0)
-                return "0" + suffix [0];
+                return "0 " + suffix [0];
             int place = Convert.ToInt32 (Math.Floor (Math.Log (bytes, 1024)));
             double num = Math.Round (bytes / Math.Pow (1024, place), 1);
-            return num + suffix [place];
+            return num + " " + suffix [place];
         }
     }
 }
