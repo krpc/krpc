3504a57a769e6f797e225f6945a4e23d43736251
djungelorm <djungelorm@users.noreply.github.com>
2026-07-21 22:44:47 +0100

    Fix script to work from any working directory

--- tools/krpc-version.sh
+++ tools/krpc-version.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-cat config.bzl | grep '^version = ' | sed -n -e "s/version\s*=\s*\"\(.*\)\"/\1/p"
+root="$(dirname "$(dirname "$(readlink -f "$0")")")"
+
+cat "$root/config.bzl" | grep '^version = ' | sed -n -e "s/version\s*=\s*\"\(.*\)\"/\1/p"
