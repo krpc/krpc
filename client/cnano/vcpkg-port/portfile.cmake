6bdd027c49c8ed3957bfc93a9b4d491d8cd462d8
djungelorm <djungelorm@users.noreply.github.com>
2026-07-22 22:46:14 +0100

    Fix port file paths to use correct name (krpc-cnano, not krpc_cnano)

--- client/cnano/vcpkg-port/portfile.cmake
+++ client/cnano/vcpkg-port/portfile.cmake
@@ -22,5 +22,5 @@ vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/krpc_cnano")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING.LESSER")
