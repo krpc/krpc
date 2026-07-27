52464748b86f17b31ea1197cdf7a19b75afc70b2
djungelorm <djungelorm@users.noreply.github.com>
2026-07-27 21:26:55 +0100

    Add the KRPC.ObjectDestroyedException exception type
    Raised when the game object that a service object refers to no longer
    exists. It is service agnostic, so it is declared against the KRPC
    service in core, and has no mapped exception because it does not
    correspond to a BCL exception type.
    
    Nothing raises it yet.

--- core/test/Service/KRPC/KRPCTest.cs
+++ core/test/Service/KRPC/KRPCTest.cs
@@ -144,10 +144,13 @@ namespace KRPC.Test.Service.KRPC
                 } else if (exception.Name == "ArgumentOutOfRangeException") {
                     MessageAssert.HasDocumentation (exception,
                         "<doc>\n<summary>\nThe value of an argument is outside the allowable range of values as defined by the invoked method.\n</summary>\n</doc>");
+                } else if (exception.Name == "ObjectDestroyedException") {
+                    MessageAssert.HasDocumentation (exception,
+                        "<doc>\n<summary>\nThe game object that a service object refers to no longer exists,\nso the object cannot be used. For example, the part it represents\nwas destroyed, or the vessel it belongs to was recovered.\n</summary>\n</doc>");
                 }
                 foundExceptions++;
             }
-            Assert.AreEqual (4, foundExceptions);
+            Assert.AreEqual (5, foundExceptions);
         }
 
         [Test]
