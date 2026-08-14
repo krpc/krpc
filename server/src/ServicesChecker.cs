b308f3f1e3559e4bad415a2047ff477b3089c693
djungelorm <djungelorm@users.noreply.github.com>
2026-08-14 23:59:10 +0100

    Remove the unreachable documentation check from the services checker
    The services checker could report undocumented services, classes, enumerations
    and procedures in a popup on game load, gated on CheckDocumented. The setting
    that turned it on was removed along with the rest of its configuration, leaving
    the property with no writer, so the check has not run since.
    
    The build checks this instead: doc:check-documented fails a build in which an
    RPC is undocumented, for each client language.

--- server/src/ServicesChecker.cs
+++ server/src/ServicesChecker.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using KRPC.Service;
 using KRPC.Service.Scanner;
 using UnityEngine;
 
@@ -15,10 +13,6 @@ namespace KRPC
     {
         internal static bool OK { get; private set; }
 
-        internal static bool CheckDocumented { get; set; }
-
-        readonly List<string> notDocumented = new List<string> ();
-
         /// <summary>
         /// Start the services checker addon
         /// </summary>
@@ -27,7 +21,7 @@ namespace KRPC
             Addon.InitLogger ();
             OK = true;
             var errors = new List<string>();
-            var services = Scanner.GetServices (errors);
+            Scanner.GetServices (errors);
             if (errors.Any()) {
                 OK = false;
                 Utils.Logger.WriteLine("Service errors encountered, plugin has been disabled. Errors were:", Utils.Logger.Severity.Error);
@@ -38,59 +32,6 @@ namespace KRPC
                     "Service errors encountered, plugin has been disabled. See the log for more information.",
                     "OK", true, HighLogic.UISkin);
             }
-            if (CheckDocumented)
-                CheckDocumentation (services.Values);
-        }
-
-        void CheckDocumentation (IEnumerable<ServiceSignature> services)
-        {
-            foreach (var service in services)
-                CheckDocumentation (service);
-            if (notDocumented.Count > 0) {
-                var n = notDocumented.Count;
-                var msg = n + " item" + (n != 1 ? "s are" : " is") + " not documented.";
-                for (int i = 0; i < 10 && i < n; i++)
-                    msg += Environment.NewLine + notDocumented [i];
-                if (n > 10)
-                    msg += Environment.NewLine + "...";
-                PopupDialog.SpawnPopupDialog(
-                    new Vector2 (0.5f, 0.5f), new Vector2 (0.5f, 0.5f), "krpc-service-warning",
-                    "kRPC service warning", msg, "OK", true, HighLogic.UISkin);
-            }
-        }
-
-        void CheckDocumentation (ServiceSignature service)
-        {
-            CheckDocumentation (service.Name, service.Documentation);
-            foreach (var cls in service.Classes.Values)
-                CheckDocumentation (cls);
-            foreach (var enm in service.Enumerations.Values)
-                CheckDocumentation (enm);
-            foreach (var proc in service.Procedures.Values)
-                CheckDocumentation (proc);
-        }
-
-        void CheckDocumentation (ProcedureSignature proc)
-        {
-            CheckDocumentation (proc.FullyQualifiedName, proc.Documentation);
-        }
-
-        void CheckDocumentation (ClassSignature cls)
-        {
-            CheckDocumentation (cls.FullyQualifiedName, cls.Documentation);
-        }
-
-        void CheckDocumentation (EnumerationSignature enm)
-        {
-            CheckDocumentation (enm.FullyQualifiedName, enm.Documentation);
-            foreach (var enmValue in enm.Values)
-                CheckDocumentation (enmValue.FullyQualifiedName, enmValue.Documentation);
-        }
-
-        void CheckDocumentation (string fullyQualifiedName, string documentation)
-        {
-            if (documentation.Length == 0)
-                notDocumented.Add (fullyQualifiedName);
         }
     }
 }
